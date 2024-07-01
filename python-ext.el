088eaadefa424fc53c553467da459473a8840b51
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-07-01 11:43:50 -0500

    [UPDATE] Python-ext: Update python-ext-docstring
    Save original position in buffer. The buffer is
    set in Markdown mode with auto-fill enabled. And
    C-c C-c is bound to the exit function. Speaking of
    which, it inserts the buffer text and restores the
    original postition.

--- python-ext.el
+++ python-ext.el
@@ -1,8 +1,11 @@
+;;; python-ext.el --- Python extension -*- lexical-binding: t; -*-
+
 (eval-and-compile
   (require 'python-mode))
 
 (eval-when-compile
-  (require 'python))
+  (require 'python)
+  (require 'debug-ext))
 
 (defcustom python-ext-docstring-fill-column 60
   "Fill column for docstring."
@@ -13,29 +16,74 @@
 ;;   "Indentation offset for python."
 ;;   :type 'integer
 ;;   :group 'user-extensions)
+(defvar user-ext-python--orig-position nil
+  "Position in the original buffer when editing Python docstring.")
+
+(defvar user-ext-python--docstring-buffer nil
+  "Buffer for Python docstring.")
 
 ;;; Functions
 
-(defun python-ext-docstring-ctr-c-ctr-c ()
+(defun python-ext--write-docstring ()
   "Exit the Python docstring buffer and apply the changes."
   (interactive)
-  (if (use-region-p)
-      (deactivate-mark))
-  (kill-region (point-min) (point-max))
-  (kill-buffer)
-  (delete-window)
-  (yank))
+  (let (num-lines prefix buffer-string)
+    (deactivate-mark)
+    (setq num-lines (count-lines (point-min) (point-max))) ; count number of lines
+    ;; extract contents of buffer into a variable
+    (with-current-buffer user-ext-python--docstring-buffer
+      (setq buffer-string (buffer-string)))
+    ;; exit function if string is empty
+    (when (string-blank-p buffer-string)
+      (kill-buffer)
+      ;; (delete-window)
+      (jump-to-register py--edit-register)
+      (error "Docstring is empty"))
+    ;; (kill-region (point-min) (point-max))
+    (kill-buffer)
+    ;; (delete-window)
+    (jump-to-register py--edit-register)
+    (insert buffer-string)
+    (assert (markerp user-ext-python--orig-position))
+    (goto-char user-ext-python--orig-position) ; go back to original position
+    (save-excursion
+      ;; get leading spaces
+      (beginning-of-line)
+      (setq prefix
+	    (if (looking-at "^\\([ \t]+\\)")
+		(match-string 1)
+	      "")))
+    ;; go to the beginning of each line and indent it
+    ;; starting on the second line
+    (move-beginning-of-line 2)
+    (dotimes (i (1- num-lines))
+      (if (looking-at-p "[ \t]*\n")	; check if line is blank
+	  (move-beginning-of-line 2)	; if blank, go to next line
+	(insert prefix)			; otherwise, insert prefix
+	(move-beginning-of-line 2)))
+    (setq py--edit-register nil)
+    (setq user-ext-python--orig-position nil)))
 
 (defun python-ext-docstring ()
   "Open a temporary buffer to write a docstring."
   (interactive)
+  (window-configuration-to-register py--edit-register)
+  ;; save position in original buffer
+  (setq user-ext-python--orig-position (point-marker))
+  ;; create a temporary buffer
+  (setq user-ext-python--docstring-buffer (tmpbuf "python-docstring"))
+  (assert (not (null user-ext-python--docstring-buffer)))
+  ;; Split window and enter temporary buffer
   (split-window-sensibly)
-  (tmpbuf "python-docstring" t)
-  (indented-text-mode)
+  (switch-to-buffer user-ext-python--docstring-buffer)
+  (markdown-mode)
+  ;; set fill column and enable auto-fill
   (set-fill-column python-ext-docstring-fill-column)
-  (auto-fill-mode)
-  (setq-local indent-tabs-mode nil)
-  (local-set-key (kbd "C-c C-c") #'python-ext-docstring-ctr-c-ctr-c))
+  (auto-fill-mode 1)
+  ;; set C-c C-c key to exit
+  (local-set-key (kbd "C-c C-c") #'python-ext--write-docstring)
+  (setq header-line-format "Python Docstring: Type C-c C-c to apply changes")
+  (message "Type C-c C-c to save changes."))
 
 (defun python-ext-scratch ()
   "Opens a scratch buffer to let you write Python code."
@@ -52,21 +100,25 @@
   (forward-line -1)
   (py-indent-or-complete))
 
-;; Key bindings
+;;; Key bindings
 
 (eval-and-compile
   (define-prefix-command 'python-ext-command-prefix nil "Python Ex")
-  (define-key python-mode-map (kbd "C-c i") #'python-ext-command-prefix)
-  (define-key python-ext-command-prefix "D" #'python-ext-docstring)
-  (define-key python-ext-command-prefix "c" #'python-skeleton-class)
-  (define-key python-ext-command-prefix "d" #'python-skeleton-def)
-  (define-key python-ext-command-prefix "f" #'python-skeleton-for)
-  (define-key python-ext-command-prefix "I" #'python-skeleton-if)
-  (define-key python-ext-command-prefix "m" #'python-skeleton-import)
-  (define-key python-ext-command-prefix "T" #'python-skeleton-try)
-  (define-key python-ext-command-prefix "w" #'python-skeleton-while)
+
+  (define-key python-mode-map (kbd "C-c i")      #'python-ext-command-prefix)
+  (define-key python-ext-command-prefix "D"      #'python-ext-docstring)
+  (define-key python-ext-command-prefix "c"      #'python-skeleton-class)
+  (define-key python-ext-command-prefix "d"      #'python-skeleton-def)
+  (define-key python-ext-command-prefix "f"      #'python-skeleton-for)
+  (define-key python-ext-command-prefix "I"      #'python-skeleton-if)
+  (define-key python-ext-command-prefix "m"      #'python-skeleton-import)
+  (define-key python-ext-command-prefix "T"      #'python-skeleton-try)
+  (define-key python-ext-command-prefix "w"      #'python-skeleton-while)
   (define-key python-mode-map (kbd "<S-return>") #'python-ext-shift-return)
-  (define-key python-mode-map (kbd "M-e") #'yas-expand))
+  (define-key python-mode-map (kbd "M-e")        #'yas-expand)
+
+  ;; company-capf
+  (define-key python-mode-map (kbd "M-SPC") #'company-capf))
 
 ;;; Hook
 
