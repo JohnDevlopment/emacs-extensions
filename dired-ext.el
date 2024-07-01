78453119f54e0f3b426c6c31bad5865b23e201ff
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-07-01 13:35:36 -0500

    [REFACTOR] Dired-ext: dired-alternate documentation

--- dired-ext.el
+++ dired-ext.el
@@ -6,14 +6,15 @@
 
 
 (defun dired-alternate (dirname &optional switches)
-  "Calls `dired' with the provided arguments, with the additional function of
-killing the current buffer. In that sense, it acts like `find-alternate-file'.
+  "\"Edit\" directory DIRNAME, same as `dired', kill the current buffer.
 
-Quick summary of options: DIRNAME is either a string or a cons, as a string, the
-name of the directory to edit; and as a cons, the first element is the
-directory, and the rest are a list of files to make directory entries for. In
-this case, SWITCHES are applied to each file individually, so list-sorting
-options are pointless."
+DIRNAME is either a string or a cons: as a string, the name
+of the directory to edit; as a cons, the first element is
+the directory, and the rest are a list of files to make
+directory entries for.
+
+In this case, SWITCHES are applied to each file
+individually, so list-sorting options are pointless."
   (interactive (dired-read-dir-and-switches ""))
   (let ((curbuf (current-buffer))
 	(newbuf (dired-noselect dirname switches)))
