86e513f09bf5139578bf0f5ed41730f6c7e11657
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 16:33:01 -0500

    [UPDATE] python-sphinx-ext.el

--- python-sphinx-ext.el
+++ python-sphinx-ext.el
@@ -1,66 +1,334 @@
-;; python-sphinx minor mode
+;; -*- lexical-binding: t; -*-
+
+(require 'sphinx-doc)
 
 (eval-when-compile
-  (require 'sphinx-doc))
+  (require 'python-mode)
+  (require 'debug-ext))
 
-(defun sphinx-mode--extra-hook ()
-  (define-key sphinx-doc-mode-map (kbd "C-c i d") #'sphinx-doc)
-  (define-key sphinx-doc-mode-map (kbd "C-c M-r") #'sphinx-add-reference))
+;;; Code:
 
-(add-hook 'sphinx-doc-mode-hook #'sphinx-mode--extra-hook)
+;; Customization
 
-;; (defun sphinx-add-keyword (type name &optional param)
-;;   "Add a keyword argument at"
-;;   (interactive "sType: \nsName:\nP"))
+;; (defgroup sphinx-ext nil
+;;   "Group for `sphinx-doc-mode' extension."
+;;   :group 'user-extensions)
 
-(defun sphinx-add-reference (&optional param)
-  "Add a reference at point. If the prefix arg PARAM is non-nil, the alternative
-form of the ref is inserted, if it has one."
+(defconst user-ext-sphinx-param-regexp
+  (rx bol (* whitespace) ?:
+      (or "keyword" "param")
+      whitespace
+      (+? (not whitespace))
+      (? whitespace (group (+? (not whitespace))))
+      ?: (+ whitespace) (not whitespace))
+  "Regular expression for :param: and :keyword: roles.")
+
+;; Functions
+
+(defun sphinx-ext--style-map-prompt ()
+  (concat
+   (format ", i = %s"
+	   (propertize "italics" 'face 'italic))
+   (format ", c = %s"
+	   (propertize "code" 'face 'font-lock-type-face))
+   (format ", C = %s"
+	   (propertize "code block" 'face 'font-lock-type-face))))
+
+(defun sphinx-ext--role-map-prompt ()
+  (let ((type-face 'font-lock-type-face))
+    (concat
+     (format "r = %s" (propertize ":returns:" 'face type-face))
+     (format ", R = %s" (propertize ":rtype:" 'face type-face))
+     (format ", k = %s" (propertize ":keyword:" 'face type-face))
+     (format ", p = %s" (propertize ":param:" 'face type-face))
+     (format ", t = %s" (propertize ":type:" 'face type-face)))))
+
+(defun sphinx-ext--insert-style (what)
+  "Insert a style at point.
+WHAT is a symbol naming what style to use:
+* 'code: inline code
+* 'italic : italics"
+  (let ()
+    (pcase what
+      ('code
+       (insert "````")
+       (left-char 2))
+      ('code-block
+       (sphinx-ext-skeleton-code-block))
+      ('italic
+       (insert "``")
+       (left-char)))))
+
+(defmacro sphinx-ext-define-insert-style-function (name key symbol doc)
+  (declare (indent 1))
+  (let* ((function-name (intern
+			 (format "sphinx-ext-insert-style-%s" name))))
+    `(progn
+       (defun ,function-name ()
+	 ,doc
+	 (interactive)
+	 (sphinx-ext--insert-style ',symbol))
+       (define-key sphinx-ext-style-map ,key #',function-name))))
+
+(defmacro sphinx-ext-define-insert-role-function (name &optional prompt &rest props)
+  "Define a function to insert a Sphinx role at point.
+The function will be bound to sphinx-ext-insert-role-NAME."
+  (let* ((function-name (intern
+			 (format "sphinx-ext-insert-role-%s" name)))
+	 (int-prompt (when prompt (format "s%s" prompt)))
+	 (key (plist-get props :key)))
+    (if prompt
+	`(progn
+	   (defun ,function-name (arg)
+	     ,(format "Insert a :%s: role at point." name)
+	     (interactive ,int-prompt)
+	     (insert (format ":%s %s: " ,name arg)))
+	   ,(when key
+	      `(define-key sphinx-ext-role-map ,key #',function-name)))
+      `(progn
+	 (defun ,function-name ()
+	   ,(format "Insert a :%s: role at point." name)
+	   (interactive)
+	   (insert (format ":%s: " ,name)))
+	 ,(when key
+	    `(define-key sphinx-ext-role-map ,key #',function-name))))))
+
+(defun sphinx-ext-add-reference (&optional arg)
+  "Add a reference at point.
+If the prefix arg ARG is non-nil, the alternative form of
+the ref is inserted, if it has one."
   (interactive "P")
-  (sphinx-add-reference--next-param param))
+  (sphinx-ext-add-reference--next-arg arg))
 
-(defun sphinx-add-reference--next-param (param)
-  (let (char)
-    (setq char (read-char-from-minibuffer "a = attribute, c = class, e = exception, f = function, m = method, r = ref: "
-					  '(?a ?c ?e ?f ?m ?r)))
+(defun sphinx-ext-add-reference--next-arg (arg)
+  "Process the next argument.
+Called from `sphinx-ext-add-reference'.  ARG is the prefix
+arg of `sphinx-ext-add-reference', which see."
+  (let ((prompt "a = attribute, c = class, e = exception, f = function, m = method, r = ref: ")
+	(chars '(?a ?c ?e ?f ?m ?r))
+	char)
+    (setq char (read-char-from-minibuffer prompt chars))
     (cond
      ((= char ?c)
       ;; insert :py:class:`...`
-      (sphinx--add-py "class" param))
+      (sphinx-ext--add-py "class"))
      ((= char ?e)
       ;; insert :py:exc:`...`
-      (sphinx--add-py "exc" param))
+      (sphinx-ext--add-py "exc"))
      ((= char ?f)
       ;; insert :py:func:`...`
-      (sphinx--add-py "func" param))
+      (sphinx-ext--add-py "func"))
      ((= char ?m)
       ;; insert :py:meth:`...`
-      (sphinx--add-py "meth" param))
+      (sphinx-ext--add-py "meth"))
      ((= char ?a)
       ;; insert :py:attr:`...`
-      (sphinx--add-py "attr" param))
+      (sphinx-ext--add-py "attr"))
      ((= char ?r)
       ;; insert :ref:`...`
-      (if current-prefix-arg
-	  (sphinx--add-ref-with-link)
-	(sphinx--add-ref)))
+      (if arg
+	  (sphinx-ext--add-ref-with-link)
+	(sphinx-ext--add-ref)))
      (t
       (error (format "invalid character %c" char))))))
 
-(defun sphinx--add-ref ()
+(defun sphinx-ext--add-ref ()
+  "Add a reference to an arbitrary location in any document.
+Prompts the user for the label."
   (let ((label (read-from-minibuffer "Label: "))
 	(buf (current-buffer)))
     (princ (format ":ref:`%s`" label) buf)))
 
-(defun sphinx--add-ref-with-link ()
+(defun sphinx-ext--add-ref-with-link ()
+  "Add a reference to an arbitrary location in any document.
+Prompts the user for a label, a link, and a title."
   (let ((label (read-from-minibuffer "Label: "))
 	(link (read-from-minibuffer "Link: "))
 	(title (read-from-minibuffer "Title: "))
 	(buf (current-buffer)))
     (princ (format ":ref:`%s %s <%s>`" link title label) buf)))
 
-(defun sphinx--add-py (field &optional param)
+(defun sphinx-ext--add-py (field)
+  "Insert a Python-domain role at point.
+FIELD is the name of the role being inserted.  Additionally,
+prompts the user for the content of the role."
   (let ((rolename (format ":py:%s:" field))
 	(ref (read-from-minibuffer "Ref: "))
 	(buf (current-buffer)))
     (princ (format "%s`%s`" rolename ref) buf)))
+
+(defun sphinx-ext-align ()
+  "Align the current line with the first line of this role.
+
+Point should be on the second line of a ':param:' or
+':keyword:' role after `auto-fill-mode' kicks in, for best
+effect."
+  (interactive)
+  (let ((pos (point-marker))
+	beg end indent)
+    (save-excursion
+      (save-match-data
+	(unless (re-search-backward user-ext-sphinx-param-regexp nil t)
+	  (user-error "Did not find a :param/keyword: here"))
+	(setq beg (match-beginning 0) end (match-end 0)
+	      indent (- (1- end) beg))
+	(assert (> end beg))
+	(goto-char pos)
+	(beginning-of-line)
+	(insert (make-string indent ?\ ))))))
+
+(defun sphinx-ext--y-or-n-p (prompt)
+  (let* ((prompt (format "%s y/n " prompt))
+	 (answer (char-to-string
+		  (read-char-choice prompt (list ?y ?n)))))
+    (string= answer "y")))
+
+(defun sphinx-ext--option (option indent type &optional optional)
+  "Insert an option to a directive.
+OPTION is an option (i.e., \":linenos:\").  INDENT is the
+indent of the option as an integer.  TYPE is a symbol
+controlling the type of the argument to OPTION.
+
+If ASK-YES-NO is non-nil, the user is asked whether they
+want to add this option."
+  nil
+  (let* ((indent (make-string indent ?\ ))
+	 (prompt (format "Insert %s? " option))
+	 res
+	 argument)
+    (if optional
+	(and (sphinx-ext--y-or-n-p
+	      (format "Insert %s?" option))
+	     res)
+      (setq argument
+	    (pcase type
+	      ('bool
+	       (sphinx-ext--y-or-n-p option))
+	      ('string
+	       (read-minibuffer
+		(format "%s: " option))))
+	    res (if (eq type 'bool)
+		    (and argument (format "%s:%s:" indent option))
+		  (format "%s:%s: %s" indent option argument))))))
+
+;; Skeletons
+
+(defmacro sphinx-ext-define-skeleton (name doc &rest skel)
+  "Define a `sphinx-doc-mode' skeleton using NAME DOC and SKEL.
+The skeleton will be bound to sphinx-ext-skeleton-NAME."
+  (declare (indent 1))
+  (let* ((name (symbol-name name))
+	 (function-name (intern (concat "sphinx-ext-skeleton-" name))))
+    `(progn
+       (define-abbrev python-mode-abbrev-table ,name "" #',function-name :system t)
+       (define-skeleton ,function-name
+	 ,doc
+	 ,@skel))))
+
+(defmacro sphinx-ext-define-auxiliary-skeleton (name &optional doc &rest skel)
+  "Define an auxiliary skeleton for `sphinx-doc-mode'.
+The skeleton is defined with NAME DOC and SKEL.  The
+skeleton is bound to sphinx-ext--skeleton-NAME."
+  (declare (indent 1))
+  (let* ((name (symbol-name name))
+	 (function-name (intern (concat "sphinx-ext-skeleton--" name)))
+	 (msg (format "Add %s? " name)))
+    (when (not skel)
+      ;; Default skeleton
+      (setq skel
+	    `(< ,(format "%s: " name) \n \n
+		> _ \n)))
+    `(progn
+       (define-skeleton ,function-name
+	 ,(or doc (format "Add %s." name))
+	 (unless (y-or-n-p ,msg)
+	   (signal 'quit t))
+	 ,@skel))))
+
+(sphinx-ext-define-auxiliary-skeleton return
+  "Insert a ':returns:'."
+  ":returns: " str)
+
+(sphinx-ext-define-skeleton docstring
+  "Insert a Sphinx-style docstring at point."
+  "First line: "
+  str \n \n
+  ("Argument: "
+   ":param " str ": " \n \n)
+  '(sphinx-ext-skeleton--return))
+
+(sphinx-ext-define-skeleton code-block
+  "Insert a code block."
+  "Language: "
+  ".. code:: " str \n
+  _ \n)
+
+(sphinx-ext-define-skeleton literalinclude
+  "Insert a directive to include code from another file."
+  "File: "
+  ".. literalinclude:: " str \n
+  (sphinx-ext--option "language" 3 'string) \n
+  (sphinx-ext--option "emphasize-lines" 3 'string) & \n
+  (sphinx-ext--option "linenos" 3 'bool))
+
+;; Key bindings
+
+(define-prefix-command 'sphinx-ext-role-map nil (sphinx-ext--role-map-prompt))
+(define-key sphinx-doc-mode-map (kbd "C-c C-R") #'sphinx-ext-role-map)
+
+(define-prefix-command 'sphinx-ext-style-map nil (sphinx-ext--style-map-prompt))
+(define-key sphinx-doc-mode-map (kbd "C-c C-S") #'sphinx-ext-style-map)
+
+(sphinx-ext-define-insert-role-function "returns" nil :key "r")
+
+(sphinx-ext-define-insert-role-function "rtype" nil :key "R")
+
+(sphinx-ext-define-insert-role-function "param" "Name: " :key "p")
+
+(sphinx-ext-define-insert-role-function "type" "Parameter: " :key "t")
+
+(sphinx-ext-define-insert-role-function "keyword" "Keyword: " :key "k")
+
+(sphinx-ext-define-insert-style-function
+    "italics" "i" italic "Insert italics at point.")
+
+(sphinx-ext-define-insert-style-function
+    "code" "c" code "Insert inline code at point.")
+
+(sphinx-ext-define-insert-style-function
+    "code-block" "C" code-block "Insert a code block at point.")
+
+(define-key sphinx-doc-mode-map (kbd "C-c i d") #'sphinx-ext-skeleton-docstring)
+(define-key sphinx-doc-mode-map (kbd "C-c M-r") #'sphinx-ext-add-reference)
+(define-key sphinx-doc-mode-map (kbd "<backtab>") #'sphinx-ext-align)
+
+;; Menu
+
+(easy-menu-define user-ext-sphinx-doc-menu-map sphinx-doc-mode-map
+  "Sphinx Minor Mode"
+  '("Sphinx"
+    ["Add reference" sphinx-ext-add-reference]
+    ["Align" sphinx-ext-align]
+    "---"
+    ("Insert Roles"
+     [":keyword:" sphinx-ext-insert-role-keyword]
+     [":param:" sphinx-ext-insert-role-param]
+     [":returns:" sphinx-ext-insert-role-returns]
+     [":rtype:" sphinx-ext-insert-role-rtype]
+     [":type:" sphinx-ext-insert-role-type])
+    ("Style"
+     ["Code" sphinx-ext-insert-style-code]
+     ["Italics" sphinx-ext-insert-style-italics]
+     ["Code Block" sphinx-ext-insert-style-code-block])
+    ("Skeletons"
+     ["Docstring" sphinx-ext-skeleton-docstring]
+     ["Literal include" sphinx-ext-skeleton-literalinclude])))
+
+;; Hooks
+
+(defun sphinx-doc-mode--extra-hook ()
+  "Hook for `sphinx-doc-mode'."
+  (setq-local skeleton-further-elements
+	      '((^ (- (1+ (current-indentation)))))))
+
+(add-hook 'sphinx-doc-mode-hook #'sphinx-doc-mode--extra-hook)
