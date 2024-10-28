32e3aeab7472386e81e8bb2474c140146dfcdee4
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 09:24:28 -0500

    [REFACTOR] general.el

--- general.el
+++ general.el
@@ -1,4 +1,10 @@
-;; -*- lexical-binding: t; -*-
+;;; general --- General extension functions.
+
+;;; Commentary:
+
+;;; Code:
+
+;;; Abbreviations
 
 (eval-and-compile
   (defvar-local user-ext-local-position-ring nil
@@ -8,6 +14,8 @@
 ;;; Functions
 
 (defun save-current-position (&optional pos)
+  "Save POS to the local position ring.
+Unless POS is provided, point is used."
   (interactive)
   (let ((pos (or pos (point-marker))))
     (add-to-history 'user-ext-local-position-ring pos)
@@ -16,6 +24,9 @@
       "Position saved to local position ring. Go back with `\\[pop-saved-position]'."))))
 
 (defun pop-saved-position ()
+  "Move point to the last saved position.
+This pops the last-saved position from
+`user-ext-local-position-ring'."
   (interactive)
   (let (pos)
     (assert (> (length user-ext-local-position-ring) 0)
@@ -25,19 +36,20 @@
     (message "Restored to position %s." pos)))
 
 (defun add-mode-comment (mode)
-  "Insert a comment line that changes the major mode to MODE.
+  "Insert a comment line to change the major mode to MODE.
 When called interactively, it prompts the user for MODE."
   (interactive "sMode: ")
   (insert (format "# -*- mode: %s; -*-" mode)))
 
 (defun bind-fill-region ()
-  "Binds `fill-region' to M-F."
+  "Bind `fill-region' to M-F."
   (interactive)
   (local-set-key (kbd "M-F") #'fill-region)
   (message "Bind `fill-region' to M-F."))
 
 (defun copy-line ()
-  "Call `kill-ring-save' on the current line from point."
+  "Copy characters from point to the end of the line.
+Unlike `kill-line', this does not delete the characters."
   (interactive)
   (let ((pos (point-marker)))
     (end-of-line)
@@ -50,7 +62,7 @@ START and END are expected to come directly from the region.
 Call `count-words-region' and"
   (interactive "r")
   (if (not (use-region-p))
-      (error "region required")
+      (error "Region required")
     (count-words--message "Region" start end)
     (setq deactivate-mark t)
     nil))
@@ -61,28 +73,33 @@ Call `count-words-region' and"
   (insert (format-time-string "%Y%m%d.%H%M")))
 
 (defun enable-wrap ()
+  "Enable line wrap if it is not already."
   (interactive)
   (if (and (boundp 'visual-line-mode) visual-line-mode)
       (error "Already called this command")
     (visual-line-mode t)))
 
 (defun kill-and-quit ()
-  "Kills the current buffer and also deletes the current window.
+  "Kill the current buffer and also delete the current window.
 The equivelent of doing C-x k and C-x 0."
   (interactive)
   (kill-buffer)
   (delete-window))
 
 (defun narrow-to-region2 (start end)
-  "Narrow to region and cancel region. START and END specify the region to narrow
-to."
+  "Narrow to region and cancel region.
+START and END specify the region to narrow to."
   (interactive "r")
   (narrow-to-region start end)
   (setq deactivate-mark t))
 
-(defun p-mode ()
-  "Activate electric-pair mode. Calls `electric-pair-local-mode'."
-  (interactive)
-  (when (null electric-pair-mode)
-    (electric-pair-local-mode 1)
-    (message "localized electric pair mode activated")))
+;; (defun p-mode ()
+;;   "Activate electric-pair mode. Calls `electric-pair-local-mode'."
+;;   (interactive)
+;;   (when (null electric-pair-mode)
+;;     (electric-pair-local-mode 1)
+;;     (message "localized electric pair mode activated")))
+
+(provide 'general)
+
+;;; general ends here
