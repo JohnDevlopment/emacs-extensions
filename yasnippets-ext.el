2504d9bdace20535bc8c562ca1e43f969a6e2610
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 15:41:09 -0500

    [UPDATE] Yasnippets: Function to compile snippets

--- yasnippets-ext.el
+++ yasnippets-ext.el
@@ -7,3 +7,25 @@
 (defun yas-ext-org-very-safe-expand ()
   (let ((yas-fallback-behavior 'return-nil))
     (yas-expand)))
+
+(defun yas-ext-compile-snippet-dir (&optional is-interactive)
+  "Call this function to compile a snippet directory.
+
+The user is prompted to choose from a list of directories
+taken from `yas-snippet-dirs'. The input is then forwarded
+to `yas-compile-directory'.
+
+The prefix arg IS-INTERACTIVE is never actually used in the
+function; rather, it's checked to see whether this function
+was called interactively."
+  (interactive "p")
+  (unless is-interactive
+    (user-error "This method can only be called interactively"))
+  (let (dir)
+    (setq dir (completing-read "Top-level directory: "
+			       (yas-snippet-dirs)
+			       nil "confirm"))
+    (yas-compile-directory dir)))
+
+(eval-and-compile
+  (define-key yas-minor-mode-map (kbd "C-c & c") #'yas-ext-compile-snippet-dir))
