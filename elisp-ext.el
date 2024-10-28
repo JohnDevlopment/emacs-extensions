2422c1ee34ec57ce007c7c167ba1469c28c90d61
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 16:34:50 -0500

    [UPDATE] elisp-ext.el

--- elisp-ext.el
+++ elisp-ext.el
@@ -14,6 +14,7 @@
 	   (eval . ((message \"A message.\"))))))))"
   "Boilerplate for a .dir-locals.el file.")
 
+;;;###autoload
 (defun elisp-ext-create-dir-locals-file (file)
   "Create a .dir-locals.el file at FILE."
   (interactive
@@ -25,6 +26,7 @@
     (let ((_buf (current-buffer)))
       (insert user-ext-elisp-dir-locals-template))))
 
+;;;###autoload
 (defun elisp-ext-forward-or-backward-sexp (&optional arg)
   "Go to the matching parenthesis to the one is adjacent at point.
 With ARG, do it that many times.  A negative arg -N reverses
@@ -43,22 +45,22 @@ the direction of the motion."
 	 (forward-sexp arg)
 	 (backward-char))))
 
+(defun elisp-ext--scratch-buffer-ctrl-c-ctrl-c ()
+  (interactive)
+  (kill-region (point-min) (point-max))
+  (kill-and-quit))
+
+;;;###autoload (autoload 'elisp-ext-scratch-buffer "elisp-ext" "Create a scratch buffer for Emacs lisp code." t)
 (define-scratch-buffer-function elisp-ext-scratch-buffer "elisp" nil
   "Create a scratch buffer for Emacs lisp code."
   nil
   (emacs-lisp-mode)
-  ;; Enable electric pair mode
-  (unless (and (boundp 'electric-pair-mode) electric-pair-mode)
-    (electric-pair-local-mode t))
-  ;; Enable company mode
-  (unless (and (boundp 'company-mode) company-mode)
-    (company-mode t))
+  (unless (and (boundp 'electric-pair-mode) electric-pair-mode) ; Enable electric pair mode
+    (electric-pair-local-mode t))				;
+  (unless (and (boundp 'company-mode) company-mode) ; Enable company mode
+    (company-mode t))				    ;
   (setq header-line-format "Type C-c C-c when finished, C-x k to cancel editing.")
-  (local-set-key (kbd "C-c C-c")
-		 (lambda ()
-		   (interactive)
-		   (kill-region (point-min) (point-max))
-		   (kill-and-quit))))
+  (local-set-key (kbd "C-c C-c") #'elisp-ext--scratch-buffer-ctrl-c-ctrl-c))
 
 (defun elisp-ext--find-quote (&optional reverse)
   "Find the next quote.
@@ -73,35 +75,35 @@ If REVERSE is non-nil, search backwards."
       (search-forward "\"")
       (point))))
 
+(defun elisp-ext-doc-scratch-buffer--shift-return ()
+  (interactive)
+  (newline)
+  (set-fill-column 60))
+
+(defun elisp-ext-doc-scratch-buffer--ctrl-c-ctrl-c ()
+  (interactive)
+  (let* ((beg (1+ (elisp-ext--find-quote)))
+	 (end (1- (elisp-ext--find-quote t))))
+    (kill-region beg end)
+    (kill-and-quit)))
+
+;;;###autoload (autoload 'elisp-ext-doc-scratch-buffer "elisp-ext" "Create a scratch buffer for Emacs lisp docstrings.")
 (define-scratch-buffer-function elisp-ext-doc-scratch-buffer "elisp docstring" nil
   "Create a scratch buffer for Emacs lisp docstrings."
   nil
   (emacs-lisp-mode)
   (auto-fill-mode t)
   (set-fill-column 67)
-  (local-set-key (kbd "S-<return>")
-		 (lambda ()
-		   (interactive)
-		   (set-fill-column 60)
-		   (newline)))
-  ;; Enable electric pair mode
+  (local-set-key (kbd "S-<return>") #'elisp-ext-doc-scratch-buffer--shift-return)
   (unless (and (boundp 'electric-pair-mode) electric-pair-mode)
     (electric-pair-local-mode t))
-  ;; Enable company mode
   (unless (and (boundp 'company-mode) company-mode)
     (company-mode t))
   (insert ";; Fill column is set to 67. Type S-<return> to set it to 60.\n")
-  (insert "\"\n\n\"")			   ; insert quotes
-  (goto-char (1+ (elisp-ext--find-quote))) ; set point in between the quotes
+  (insert "\"\n\n\"")
+  (goto-char (1+ (elisp-ext--find-quote)))
   (setq header-line-format "Type C-c C-c when finished, C-x k to cancel editing.")
-  (local-set-key (kbd "C-c C-c")
-		 (lambda ()
-		   (interactive)
-		   (let* ((beg (1+ (elisp-ext--find-quote)))
-			  (end (1- (elisp-ext--find-quote t))))
-		     (kill-region beg end)
-		     (kill-and-quit)))))
-
+  (local-set-key (kbd "C-c C-c") #'elisp-ext-doc-scratch-buffer--ctrl-c-ctrl-c))
 
 (provide 'elisp-ext)
 
