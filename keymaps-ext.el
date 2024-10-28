1bf0d34df26795017d7ba570345f643c125a8dfd
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 16:09:23 -0500

    [UPDATE] keymaps-ext.el

--- keymaps-ext.el
+++ keymaps-ext.el
@@ -1,70 +1,107 @@
+;; -*- lexical-binding: t;  -*-
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'move-text)
 (require 'desktop+)
+(require 'ido)
 
 (move-text-default-bindings)
 (desktop+-default-bindings)
 
-(defmacro define-contextual-key-func (key &rest modes)
-  "Define a function that enables one of the modes.
-KEY is a string name for the key to press.
+(defmacro define-contextual-key-func (key &rest args)
+  "Define a function that enables one of the modes listed in ARGS.
+KEY is a string name for the key to press with the prefix
+C-c C-m.
 
-MODE is a string that is the name of a mode. If there is
-only one mode, this is expanded into a function call.
+MODE is a string that is the name of a mode.  If there is
+only one mode, this is expanded into a direct function
+call.  Otherwise, the user is prompted to pick from a list
+of modes using a completion function.
 
-\(fn KEY MODE ...)"
+\(fn KEY ARGS...)"
   (declare (indent 1))
-  (let (doc)
-    (if (= (length modes) 1)
+  (let (modes use-ido)
+    (unless args
+      (error "No args provided"))
+    ;; Collect modes from ARGS; handle keyword args
+    (while args
+      (let* ((keyword (pop args))
+	     (value (pop args)))
+	(cond
+	 ((eq keyword :ido)
+	  (unless value
+	    (error "Keyword %s is missing an argument" keyword))
+	  (setq use-ido value))
+	 (t
+	  (push keyword modes)
+	  (unless (null value)
+	    (push value modes))))))
+    (setq modes (sort modes #'string<))	; alphabetically sort mode strings
+    (if (= (length modes) 1)		; only one mode, so call directly as a function
 	`(progn
-	   ,(setq doc (format "Call `%s'.\nLikely called from C-c C-m %s."
-			      (car modes) key))
 	   (defun ,(intern (concat "contextual-key-" key)) ()
-	     ,doc
+	     ,(format "Call `%s'.\nLikely called from C-c C-m %s."
+		      (car modes) key)
 	     (interactive)
-	     ;; MODES is one mode, so call it directly
+	     ;; ARGS is one mode, so call it directly
 	     (call-interactively #',(intern (car modes)))))
       `(progn
-	 ,(setq doc (format "Choose from a list of modes to enable.\nLikely called from C-c C-m %s."
-			    key))
 	 (defun ,(intern (concat "contextual-key-" key))
 	     (choice)
-	   ,doc
-	   (interactive (list (completing-read "Choose: " ',modes)))
+	   ,(format "Choose from a list of modes to enable.\nLikely called from C-c C-m %s."
+		    key)
+	   (interactive
+	    (list
+	     ,(if use-ido
+		  `(ido-completing-read "Choose: " ',modes)
+		`(completing-read "Choose: " ',modes))))
 	   (call-interactively (intern choice)))))))
 
-(eval-and-compile
-  (define-prefix-command 'command-map)
-  (define-prefix-command 'quick-mode-map))
+(define-prefix-command 'command-map)
+(define-prefix-command 'quick-mode-map)
 
 (define-contextual-key-func "a"
+  :ido t
   "abbrev-mode"
   "auto-fill-mode"
   "auto-revert-mode")
 
-(define-contextual-key-func "c" "comment-tags-mode" "company-mode")
+(define-contextual-key-func "b"
+  "bind-imenu"
+  "bind-imenu-lsp")
+
+(define-contextual-key-func "c"
+  "comment-tags-mode"
+  "conf-mode"
+  "company-mode")
+
+(define-contextual-key-func "f"
+  "flycheck-mode")
 
 (define-contextual-key-func "g"
+  :ido t
+  "electric-pair-mode"
+  "global-comment-tags-mode"
   "global-company-mode"
-  "global-electric-pair-mode"
+  "global-flycheck-mode"
   "global-visual-line-mode"
   "yas-global-mode")
 
-(defun contextual-key-s ()
-  "Depending on the major mode, either switches to a different major mode or turns
-on a minor mode."
-  (interactive)
-  (if (eq major-mode 'python-mode)
-      (sphinx-doc-mode)
-    (shell-script-mode)))
+(define-contextual-key-func "s"
+  "shell-script-mode"
+  "sphinx-doc-mode")
 
 (define-contextual-key-func "y" "yas-minor-mode")
 
 (defun lookup-function (fn &optional keymap firstonly)
-  "Lookup the key binding for FN. If KEYMAP is non-nil,
-only search through that keymap.
+  "Lookup the key binding for FN.
+If KEYMAP is non-nil, only search through that keymap.
 
 Returns a list of keybinding descriptions unless FIRSTONLY
-is non-nill."
+is non-nil."
   (let ((all-bindings
 	 (where-is-internal (if (symbolp fn)
 				fn
@@ -81,12 +118,14 @@ is non-nill."
       (car keys))))
 
 (defun map-revert-buffer ()
+  "Map M-R to `revert-buffer'."
   (interactive)
   (local-set-key (kbd "M-R") #'revert-buffer)
   (message "Mapped %s `revert-buffer'"
 	   (lookup-function 'revert-buffer nil t)))
 
 (defun remap-narrow-to-region ()
+  "Assign the key mapped to `narrow-to-region' to `narrow-to-region2'."
   (interactive)
   (local-set-key [remap narrow-to-region] #'narrow-to-region2)
   (message "Remapped %s to `narrow-to-region2'"
@@ -107,15 +146,14 @@ is non-nill."
 ;; Prefix command for quickly turning on different modes
 (global-set-key (kbd "C-c m") #'quick-mode-map)
 
-;; (define-key quick-mode-map (kbd "a") #'abbrev-mode)
 (define-key quick-mode-map (kbd "a") #'contextual-key-a)
+(define-key quick-mode-map (kbd "b") #'contextual-key-b)
 (define-key quick-mode-map (kbd "c") #'contextual-key-c)
-;; (define-key quick-mode-map (kbd "f") #'auto-fill-mode)
+(define-key quick-mode-map (kbd "f") #'contextual-key-f)
 (define-key quick-mode-map (kbd "g") #'contextual-key-g)
 (define-key quick-mode-map (kbd "l") #'lsp-mode)
 (define-key quick-mode-map (kbd "o") #'origami-mode)
 (define-key quick-mode-map (kbd "p") #'p-mode)
-;; (define-key quick-mode-map (kbd "r") #'auto-revert-mode)
 (define-key quick-mode-map (kbd "s") #'contextual-key-s)
 (define-key quick-mode-map (kbd "v") #'view-mode)
 (define-key quick-mode-map (kbd "w") #'enable-wrap)
@@ -129,10 +167,11 @@ is non-nill."
 (define-key command-map (kbd "M-s") #'nonincremental-search-backward)
 (define-key command-map (kbd "c")   #'comment-region)
 (define-key command-map (kbd "d")   #'copy-text-down)
-(define-key command-map (kbd "p")   #'forward-or-backward-sexp)
+(define-key command-map (kbd "p")   #'elisp-ext-forward-or-backward-sexp)
 (define-key command-map (kbd "s")   #'nonincremental-search-forward)
 (define-key command-map (kbd "'")   #'save-current-position)
 (define-key command-map (kbd "&")   #'pop-saved-position)
+(define-key command-map (kbd "M")   #'add-mode-comment)
 
 ;; Misc commands which do not use my custom prefix
 (global-set-key (kbd "C-M-k")   #'copy-line)
@@ -142,3 +181,15 @@ is non-nill."
 (global-set-key (kbd "M-=")     #'count-words-region2)
 ;; (global-set-key (kbd "C-x C-b") #'list-buffers-ex)
 (global-set-key (kbd "C-x C-b") #'ibuffer)
+(global-set-key (kbd "C-x M-f") #'find-file-at-point)
+(global-set-key (kbd "C-x Q")   #'macro-ext-query)
+
+;; Extension-related commands
+(define-prefix-command 'extension-map)
+(global-set-key (kbd "C-c e") #'extension-map)
+
+(define-key extension-map "l" #'load-extension)
+(define-key extension-map "F" #'find-extension-at-point)
+(define-key extension-map "f" #'find-extension)
+
+;;; keymaps-ext.el ends here
