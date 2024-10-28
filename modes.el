bb9cbf84e37215337aa944067eef0f39f1eb3902
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-28 09:18:40 -0500

    [UPDATE] Restructure modes.el

--- modes.el
+++ modes.el
@@ -1,62 +1,29 @@
-;; Dired
+;; -*- lexical-binding: t; -*-
+
+;; Major modes
+;; (load-extension "bbcode-ext" t)
 (load-extension "dired-ext" t)
+;; (load-extension "dtext-ext" t)
+(load-extension "elisp-ext" t)
+(load-extension "js-ext" t)
+(load-extension "log4j-ext" t)
+(load-extension "markdown-ext" t)
+(load-extension "nroff-ext" t)
+;; (load-extension "org-ext" t)
+(load-extension "perl-ext" t)
+(load-extension "python-ext" t)
+(load-extension "sh-ext" t)
+(load-extension "toml-ext" t)
 
-;; Org
-(load-extension "org-ext" t)
-
-;; Markdown
-(load-extension "markdown-ext")
-
-;; DText
-(load-extension "dtext-ext" t)
-
-;; Lua
-(load-extension "lua-ext")
-
-;; Rust
-(load-extension "rust-ext")
-
-;; C/C++
-(load-extension "c-ext")
-
-;; sh
-(load-extension "sh-ext")
-
-;; yasnippets
+;; Minor modes
+;; (load-extension "comment-tags-ext" t)
+(load-extension "origami-ext" t)
+(load-extension "python-sphinx-ext" t)
 (load-extension "yasnippets-ext")
 
-;; Python
-(load-extension "python-ext")
-(load-extension "python-sphinx-ext")
-
-;; Javscript
-(add-hook 'js-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
-	    (setq tab-width 2)))
-
-;; BB-code
-(load-extension "bbcode-ext")
-
-;; Latex
-(load-extension "latex-ext")
-
-;; Elisp
-(load-extension "elisp-ext")
-
-;; Tcl
-(load-extension "tcl-ext")
-
-;; Origami
-(load-extension "origami-ext")
-
-;; Help
-
-(defun help--extra-hook ()
-  (define-key help-mode-map (kbd "C-c q") #'kill-and-quit))
-
 ;; Liquidsoap
-(load-extension "liquidsoap-bootstrap")
-(load-extension "liquidsoap-ext")
+(load-extension "liquidsoap-bootstrap" t)
+;; (load-extension "liquidsoap-ext" t)
 
-;; ====
+;; Electric Pair
+;; (load-extension "p-mode" t)
