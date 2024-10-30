f14fb6408f6fd457b3f4bfa8905fc0c683207606
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-30 09:58:08 -0500

    [REMOVE] latex-ext.el

--- latex-ext.el
+++
@@ -1,6 +0,0 @@
-;;; -*- lexical-binding: t; -*-
-
-(defun user-ext-cdlatex-mode-extra-hook ()
-  "Extra hook for `cdlatex-mode'.")
-
-(add-hook 'cdlatex-mode-hook #'user-ext-cdlatex-mode-extra-hook)
