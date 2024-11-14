06595bd62996ce3292a2f469c57ada336318092f
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-11-14 01:25:36 -0600

    [UPDATE] Python-ext: alist-ext-from-list
    Replaces alist-ext-add.

--- packages/alist-ext.el
+++ packages/alist-ext.el
@@ -23,11 +23,31 @@
 ;;; Code:
 
 (require 'debug-ext)
+(require 'cl-lib)
 
-(defmacro alist-ext-add (alist key value)
-  "Add an element to ALIST."
+(defun alist-ext-from-list (list)
+  "Turn a regular list LIST into an alist.
+LIST must have an even number of elements."
+  (unless (= (mod (length list) 2) 0)
+    (user-error "Must have even number of arguments."))
+  (seq-partition list 2))
+
+(defmacro alist-ext-define (&rest pairs)
+  "Construct an alist with PAIRS, where each key precedes its value.
+
+Each key can be any valid lisp object, but symbols have to
+be quoted.
+
+\(fn KEY VALUE ...)"
   (declare (indent 2))
-  `(append ,alist (list ,key ,value)))
+  (unless (= (mod (length pairs) 2) 0)
+    (error "alist-ext-define requires an even number of arguments (key-value pairs)."))
+  (let (al key value)
+    (while pairs
+      (setq key (pop pairs)
+	    value (pop pairs))
+      (push `(cons ,key ,value) al))
+    `(list ,@al)))
 
 (provide 'alist-ext)
 ;;; alist-ext.el ends here
