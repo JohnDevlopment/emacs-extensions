a3549f49500f4d807bb332c01eebb29d50c33e0c
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-11-25 19:53:41 -0600

    [UPDATE] Rust-ext: Major revamp
    * Added functions
    ** rust-ext-finish-variable-type
    ** rust-ext-cargo-run-with-args
    ** rust-ext-customize-group
    ** rust-ext-scratch-buffer
    ** rust-skeleton-match
    * Improvements
    ** rust-ext--write-docstring
    ** Add menu
    ** Bind functions to keys

--- rust-ext.el
+++ rust-ext.el
@@ -5,7 +5,9 @@
 ;;; Code:
 
 (eval-when-compile
+  (require 'company-capf)
   (require 'rust-mode)
+  (require 'rustic)
   (require 'skeleton))
 
 ;; Variables
@@ -26,36 +28,43 @@
 
 ;; Functions
 
+(defun rust-ext-finish-variable-type ()
+  "Complete the type of a variable using `company-capf'.
+Point must be at the end of a variable binding where the
+type usually goes."
+  (interactive)
+  (insert ": ")
+  (call-interactively #'company-capf))
+
+;;;###autoload
+(defun rust-ext-cargo-run-with-args ()
+  "Run this project via 'cargo run'.
+This calls `rustic-cargo-run' with a non-nil argument."
+  (interactive)
+  (rustic-cargo-run 4))
+
 (defun rust-ext--write-docstring ()
   "Exit the Rust docstring buffer and apply the docstring."
   (interactive)
-  (let (num-lines prefix buffer-string)
+  (let (buffer-string docstring)
     (deactivate-mark)
     (untabify (point-min) (point-max))
-    (setq num-lines (count-lines (point-min) (point-max))) ; count number of lines
-    (with-current-buffer user-ext-rust--docstring-buffer   ; extract contents of buffer
-      (setq buffer-string (buffer-string)))		   ; into a variable
-    (when (string-blank-p buffer-string)		   ; exit function if string is empty
-      (kill-buffer)					   ;
-      (jump-to-register user-ext-rust--register)	   ;
-      (error "Docstring is empty"))			   ;
+    (with-current-buffer user-ext-rust--docstring-buffer
+      (setq buffer-string (buffer-string)))
+    ;; exit if buffer is empty
+    (when (string-blank-p buffer-string)
+      (kill-buffer)
+      (jump-to-register user-ext-rust--register)
+      (error "Docstring is empty"))
     (kill-buffer)
     (jump-to-register user-ext-rust--register)
-    (insert buffer-string)
     (save-excursion
-      ;; get leading spaces
-      (beginning-of-line)
-      (setq prefix
-	    (concat "/// "
-		    (if (looking-at "^\\([ \t]+\\)")
-			(match-string 1)
-		      ""))))
-    ;; go to the beginning of each line and indent it
-    ;; starting on the second line
-    (move-beginning-of-line 2)
-    (dotimes (_i (1- num-lines))
-      (insert prefix)
-      (move-beginning-of-line 2))))
+      (setq docstring
+	    (string-join
+	     (cl-loop
+	      for line in (split-string buffer-string "\n")
+	      collect (format "/// %s" line)) "\n"))
+      (insert docstring))))
 
 ;;;###autoload
 (defun rust-ext-docstring ()
@@ -63,22 +72,34 @@
   (interactive)
   (let (modes)
     (window-configuration-to-register user-ext-rust--register)
-    (dolist (mode '(abbrev-mode))
-      (when (and (boundp mode) mode)
-	(cl-pushnew mode modes)))
+    (dolist (mode '(abbrev-mode))	; we want to enable minor modes
+      (when (and (boundp mode) mode)	; that were enabled in the
+	(cl-pushnew mode modes)))	; original buffer
     (setq user-ext-rust--docstring-buffer (tmpbuf "rust-docstring"))
     (assert (not (null user-ext-rust--docstring-buffer)))
     (split-window-sensibly)				 ;
     (switch-to-buffer user-ext-rust--docstring-buffer)   ; Split window and enter temporary buffer
     (markdown-mode)
-    (dolist (mode modes)
-      (funcall mode 1))
-    ;; set fill column and enable auto-fill
+    (dolist (mode modes)		; enable minor
+      (funcall mode 1))			; modes from earlier
     (set-fill-column user-ext-rust-docstring-fill-column)
     (auto-fill-mode 1)
     (local-set-key (kbd "C-c C-c") #'rust-ext--write-docstring)
     (setq header-line-format "Rust Docstring: Type C-c C-c to apply changes")
     (message "Type C-c C-c to save changes.")))
+
+(defun rust-ext-customize-group ()
+  "Customize the group for `rusti-mode'."
+  (interactive)
+  (customize-group 'rustic))
+
+;;;###autoload
+(define-scratch-buffer-function rust-ext-scratch-buffer "rust" nil
+  "Open a scratch buffer in `rustic-mode'."
+  nil
+  (rustic-mode))
+
+;; Skeletons
 
 (defmacro rust-skeleton-define (name doc &rest skel)
   "Define a `rust-mode' skeleton using NAME DOC and SKEL.
@@ -92,20 +113,79 @@ The skeleton will be bound to rust-skeleton-NAME."
 	 ,@skel))))
 
 (rust-skeleton-define function "Insert a function definition."
-		      "Name: "
-		      "fn " str "(" ("Parameter, %s: "
-				     (unless (equal ?\( (char-before)) ", ")
-				     str) ") {" \n
-				     > "// code..." _ \n < "}" \n)
+  "Name: "
+  "fn " str "(" ("Parameter, %s: "
+		 (unless (equal ?\( (char-before)) ", ")
+		 str) ") {" \n
+  > "// code..." _ \n < "}" \n)
+
+(rust-skeleton-define match "Insert a match expression."
+  "Expression: "
+  "match " str
+  " {" ("Case Expression, %s: "
+	\n str " => ...,")
+  resume:
+  ?\n > ?} (when (y-or-n-p "Insert \";\"? ") ?\;))
+
+;; Keymaps
 
 (define-key rust-mode-map (kbd "M-S-SPC") #'company-capf)
-(define-key rust-mode-map (kbd "C-c C-t f") #'rust-skeleton-function)
 (define-key rust-mode-map (kbd "C-c i D") #'rust-ext-docstring)
+(define-key rust-mode-map (kbd "C-c C-c M-c") #'rust-ext-finish-variable-type)
+
+(define-prefix-command 'user-ext-rust-skeleton-map)
+(define-key rust-mode-map (kbd "C-c C-t") #'user-ext-rust-skeleton-map)
+
+(define-key user-ext-rust-skeleton-map (kbd "f") #'rust-skeleton-function)
+(define-key user-ext-rust-skeleton-map (kbd "m") #'rust-skeleton-match)
+
+(define-key rust-mode-map (kbd "C-c C-j") #'imenu)
+
+(easy-menu-define user-ext-rust-menu-map rustic-mode-map
+  "Rustic Major Mode"
+  '("Rustic"
+    ("Cargo"
+     ;; Project-level build/clean commands
+     ["New Project" rustic-cargo-new]
+     ["Initialize Project" rustic-cargo-init]
+     "---"
+     ["Build Project" rustic-cargo-build]
+     ["Run Project" rustic-cargo-run]
+     ["Run Project With Args" rust-ext-cargo-run-with-args]
+     ["Check Project" rustic-cargo-check]
+     ["Run Tests On Project" rustic-cargo-test]
+     ["Run Benchmarks" rustic-cargo-bench]
+     ["Clean Project" rustic-cargo-clean]
+     "---"
+     ["Add Crate" rustic-cargo-add]
+     ["Remove Crate" rustic-cargo-rm]
+     ["List Outdated Packages" rustic-cargo-outdated]
+     ["Upgrade Dependencies" rustic-cargo-upgrade]
+     "---"
+     ["Open Documentation In Browser" rustic-cargo-doc]
+     ["Run Test At Point" rustic-cargo-current-test]
+     ["Format Code" rustic-cargo-fmt]
+     ["Lint Project With Clippy" rustic-cargo-clippy]
+     ["Fix With Clippy" rustic-cargo-clippy-fix]
+     ["Run Clippy" rustic-cargo-clippy])
+    ("Extensions"
+     ["Write Documentation" rust-ext-docstring]
+     ["Finish Variable Type" rust-ext-finish-variable-type]
+     ("Skeletons"
+      ["Function" rust-skeleton-function]
+      ["Match Statement" rust-skeleton-match]))
+    ["Compile Program" rustic-compile]
+    ["Recompile Program" rustic-recompile]
+    ["Format This Buffer" rustic-format-buffer]
+    ["Make A Documentation" rustic-docstring-dwim]
+    ["Customize This Group" rust-ext-customize-group]))
 
 ;;;###autoload
 (defun rust--custom-hook ()
+  "Hook for `rustic-mode' extension."
   (setq-local skeleton-further-elements
-	      '((< '(- (min rust-indent-offset (current-column)))))))
+	      '((< '(- (min rust-indent-offset (current-column))))))
+  (visual-line-mode t))
 
 ;;;###autoload
 (add-hook 'rust-mode-hook #'rust--custom-hook)
