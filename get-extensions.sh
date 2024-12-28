b6d771533c1ad3b48f2f896b67c2552d080a6919
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-12-27 19:26:39 -0600

    [UPDATE] get-extensions.sh
    Simplify how the source files are located. Also,
    do some minor refactoring and cleaning.

--- get-extensions.sh
+++ get-extensions.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-SRCDIR="$HOME/.emacs.d/extensions"
+SRCDIR=~/.emacs.d/extensions
 
 # Find all .el files and subdirectories; print after the first line
 # Only print, do no act on anything
-find "$SRCDIR" -maxdepth 1 -type f -iname '*.el' -o -type d | tail -n +2 |\
-    parallel -X "cp -vrft . {}"
-
-# Copy all .el files to the current directory
-# cp -v -t . "$SRCDIR"/*.el
+find $SRCDIR -maxdepth 2 -type f -name '*.el' | parallel -X "cp -vrft . {}"
