97fa40d143a6dd40a89caa391ea8686103224e0e
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-12-29 06:21:08 -0600

    [REFACTOR] sh-ext.el
    * Organize variables, functions
    * Other stuff

--- sh-ext.el
+++ sh-ext.el
@@ -7,56 +7,14 @@
 (require 'ido)
 (require 'sh-script)
 
-(defconst sh-ext-function-regex
-  "^\\(?:function\\s-+\\([A-Z_a-z-]+\\)\\|\\(?1:[A-Z_a-z-]+\\)()\\)"
-  "Regular expression for finding functions.")
+;; Variables
 
-(defun sh-ext-occur-functions (&optional nlines)
-  "Run `occur' on the current buffer for function definitions.
-Each line is matched with `sh-ext-function-regex'.
-
-Optional arg NLINES is interpreted the same way as for
-`occur'.  Interactively it is the prefix arg.
-
-Optional args BEG and END, are used to restrict search to a
-designated region.  Interactively, the region is used to set
-these args."
-  (interactive "p")
-  (let (beg end)
-    (when (use-region-p)
-      (setq beg (region-beginning) end (region-end)))
-    (occur sh-ext-function-regex
-	   (if (= nlines 1) -2 nlines)
-	   (if (use-region-p)
-	       (list beg end)))
-    (with-current-buffer "*Occur*"
-      ;; Buffer defined = kill buffer
-      (let* ((bufname "*Occur: sh functions*")
-	     (buf (get-buffer bufname)))
-	(when buf
-	  (kill-buffer bufname))
-	(highlight-parentheses-mode -1)
-	(rename-buffer bufname)
-	(local-set-key "q" #'kill-and-quit)))))
-
-(defun sh-ext--once-mods-hist (c)
-  (cond
-   ((string= c "o")
-    " (once)")
-   ((string= c "h")
-    " (once, modifies history)")
-   (t nil)))
-
-(define-skeleton sh-ext-skeleton-src-command-list
-  "Inserts a command list like is seen in \"src\"."
-  "Delimter: "
-  "cat <<" str \n
-  "Commands:"\n
-  ("Command: "
-   "  * " str (sh-ext--once-mods-hist (skeleton-read
-				       "Character (o, h): "))
-   "\n")
-  str)
+(defconst user-ext-sh-function-regex
+  "^\\s-*\\(function\\s-+\\([A-Z_a-z-]+\\)\\)\\s-*{"
+  "Regular expression for finding functions.
+Group 1 matches the first line of the declaration starting
+with the keyword 'function'.
+Group 2 matches the name of the function.")
 
 (defconst user-ext-sh-ext-color-escapes
   '((fg-black . "30")
@@ -77,6 +35,57 @@ these args."
     (bg-light-gray . "47")
     (reset . "0"))
   "Color/property names mapped to their equivalent Bash escape code.")
+
+;; Functions
+
+(defun sh-ext-mark-function ()
+  "Mark the surrounding function."
+  (interactive)
+  (let* ((ppss (syntax-ppss))
+	 (tl (syntax-ppss-toplevel-pos ppss)))
+    (goto-char tl)
+    (beginning-of-line)
+    (when (looking-at user-ext-sh-function-regex)
+      (goto-char (match-beginning 1))
+      (activate-mark)
+      (goto-char tl)
+      (forward-sexp))))
+
+(defun sh-ext-occur-functions (&optional nlines)
+  "Run `occur' on the current buffer for function definitions.
+Each line is matched with `user-ext-sh-function-regex'.
+
+Optional arg NLINES is interpreted the same way as for
+`occur'.  Interactively it is the prefix arg.
+
+Optional args BEG and END, are used to restrict search to a
+designated region.  Interactively, the region is used to set
+these args."
+  (interactive "p")
+  (let (beg end)
+    (when (use-region-p)
+      (setq beg (region-beginning) end (region-end)))
+    (occur user-ext-sh-function-regex
+	   (if (= nlines 1) -2 nlines)
+	   (if (use-region-p)
+	       (list beg end)))
+    (with-current-buffer "*Occur*"
+      ;; Buffer defined = kill buffer
+      (let* ((bufname "*Occur: sh functions*")
+	     (buf (get-buffer bufname)))
+	(when buf
+	  (kill-buffer bufname))
+	(highlight-parentheses-mode -1)
+	(rename-buffer bufname)
+	(local-set-key "q" #'kill-and-quit)))))
+
+(defun sh-ext--once-mods-hist (c)
+  (cond
+   ((string= c "o")
+    " (once)")
+   ((string= c "h")
+    " (once, modifies history)")
+   (t nil)))
 
 (defun sh-ext--color-escape-complete ()
   (list
@@ -104,16 +113,22 @@ it is non-nil, \"1;\" is prepended to the color code."
 	    (format "\\e[%sm" (cdr (assq color user-ext-sh-ext-color-escapes)))))
     (insert result)))
 
-(define-scratch-buffer-function sh-ext-scratch-buffer "sh scratch" nil
-  "Open a scratch buffer to edit Shell script."
-  nil
-  (sh-mode))
-
 (defun sh-ext-insert-non-printing-escape ()
   "Insert Bash non-printing escape sequence."
   (interactive)
   (insert "\\[\\]")
   (left-char 2))
+
+(define-skeleton sh-ext-skeleton-src-command-list
+  "Inserts a command list like is seen in \"src\"."
+  "Delimter: "
+  "cat <<" str \n
+  "Commands:"\n
+  ("Command: "
+   "  * " str (sh-ext--once-mods-hist (skeleton-read
+				       "Character (o, h): "))
+   "\n")
+  str)
 
 ;; Hook
 
@@ -129,10 +144,9 @@ it is non-nil, \"1;\" is prepended to the color code."
 (add-hook 'sh-mode-hook #'sh--extra-hook)
 
 ;; Key bindings and abbrevs
-(eval-and-compile
-  (define-abbrev sh-mode-abbrev-table "cmds" "" #'sh-ext-skeleton-src-command-list)
-  (define-key sh-mode-map (kbd "C-c \\") #'sh-ext-color-escape)
-  (define-key sh-mode-map (kbd "C-c [") #'sh-ext-insert-non-printing-escape))
+(define-abbrev sh-mode-abbrev-table "cmds" "" #'sh-ext-skeleton-src-command-list)
+(define-key sh-mode-map (kbd "C-c \\") #'sh-ext-color-escape)
+(define-key sh-mode-map (kbd "C-c [") #'sh-ext-insert-non-printing-escape)
 
 (provide 'sh-ext)
 
