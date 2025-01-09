9e765adfd7ce4186f7e842f835f012c194d06103
JohnDevlopment <johndevlopment7@gmail.com>
2025-01-09 13:32:23 -0600

    [ADD] help-ext.el

---
+++ help-ext.el
@@ -0,0 +1,39 @@
+;;; help-ext.el --- Help key bindings                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  John Russell
+
+;; Author: John Russell <john@john-110-210>
+;; Keywords: extensions
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(eval-when-compile
+  (require 'menu-bar))
+
+(define-prefix-command 'help-ext-command)
+(global-set-key (kbd "C-h M") #'help-ext-command)
+
+;; Subject lookup
+(define-key help-ext-command (kbd "l") #'elisp-index-search)
+(define-key help-ext-command (kbd "u") #'emacs-index-search)
+
+(provide 'help-ext)
+
+;;; help-ext.el ends here
