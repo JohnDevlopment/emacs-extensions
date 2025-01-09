1eeff75e3926b7e8648ec531c26941aae8be9ded
JohnDevlopment <johndevlopment7@gmail.com>
2025-01-09 13:32:16 -0600

    [ADD] alist-ext.el

--- alist-ext.el
+++ alist-ext.el
@@ -39,13 +39,15 @@ Each key can be any valid lisp object, but symbols have to
 be quoted.
 
 \(fn KEY VALUE ...)"
+  (declare (indent 2))
   (unless (= (mod (length pairs) 2) 0)
     (error "alist-ext-define requires an even number of arguments (key-value pairs)."))
-  (let (res key value)
+  (let (al key value)
     (while pairs
-      (setq key (pop pairs) value (pop pairs))
-      (cl-append-list (list 'cons key value) res))
-    `(list ,@res)))
+      (setq key (pop pairs)
+	    value (pop pairs))
+      (push `(cons ,key ,value) al))
+    `(list ,@al)))
 
 (provide 'alist-ext)
 ;;; alist-ext.el ends here
