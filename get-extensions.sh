34c496e24cad04041f26ef4850fe865444961889
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-01-09 23:53:06 -0600

    [UPDATE] get-extensions.sh

--- get-extensions.sh
+++ get-extensions.sh
@@ -4,4 +4,7 @@ SRCDIR=~/.emacs.d/extensions
 
 # Find all .el files and subdirectories; print after the first line
 # Only print, do no act on anything
-find $SRCDIR -maxdepth 2 -type f -name '*.el' | parallel -X "cp -vrft . {}"
+# find $SRCDIR -maxdepth 2 -type f -name '*.el' | parallel -X "cp -vrft . {}"
+
+cp -vft ./ $SRCDIR/*.el
+cp -vft ./ $SRCDIR/packages/
