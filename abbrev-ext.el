0ece7bb4598f866dc1fa485e1a06230450a04700
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-02-03 23:16:46 -0600

    [UPDATE] abbrev-ext.el

--- abbrev-ext.el
+++ abbrev-ext.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(eval-when-compile
+  (require 'abbrev))
+
 (defun abbrev-ext-insert-hook ()
   t)
 
@@ -7,3 +10,6 @@
 
 (advice-add 'add-mode-abbrev :after #'deactivate-mark)
 (advice-add 'add-global-abbrev :after #'deactivate-mark)
+
+(define-abbrev global-abbrev-table "ie" "i.e." #'abbrev-ext-insert-hook :system t)
+(define-abbrev global-abbrev-table "eg" "e.g." #'abbrev-ext-insert-hook :system t)
