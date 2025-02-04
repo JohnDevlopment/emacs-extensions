c8bb2d4f812f6c77017cff6f86736f668b5daff2
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-02-04 17:33:31 -0600

    [UPDATE] syntax-ext.el

--- syntax-ext.el
+++ syntax-ext.el
@@ -1,20 +1,20 @@
 (require 'cl-lib)
 
-(defun my/mcpyrate-syntax-highlight-setup ()
-  "Set up additional syntax highlighting for `mcpyrate` in Python mode."
-  ;; adapted from code in dash.el
-  (let ((more-keywords '("macros" "dialects"
-                         "q" "u" "n" "a" "s" "t" "h"))
-        ;; How to make Emacs recognize your magic variables. Only for the anaphoric if demo.
-        ;; A list, like `more-keywords`, even though in the example there is only one item.
-        (magic-variables '("it")))
-    (font-lock-add-keywords 'python-mode `((,(concat "\\_<" (regexp-opt magic-variables 'paren) "\\_>")
-                                            1 font-lock-variable-name-face)) 'append)
-    (font-lock-add-keywords 'python-mode `((,(concat "\\_<" (regexp-opt more-keywords 'paren) "\\_>")
-                                            1 font-lock-keyword-face)) 'append)
-    ))
+;; (defun my/mcpyrate-syntax-highlight-setup ()
+;;   "Set up additional syntax highlighting for `mcpyrate` in Python mode."
+;;   ;; adapted from code in dash.el
+;;   (let ((more-keywords '("macros" "dialects"
+;;                          "q" "u" "n" "a" "s" "t" "h"))
+;;         ;; How to make Emacs recognize your magic variables. Only for the anaphoric if demo.
+;;         ;; A list, like `more-keywords`, even though in the example there is only one item.
+;;         (magic-variables '("it")))
+;;     (font-lock-add-keywords 'python-mode `((,(concat "\\_<" (regexp-opt magic-variables 'paren) "\\_>")
+;;                                             1 font-lock-variable-name-face)) 'append)
+;;     (font-lock-add-keywords 'python-mode `((,(concat "\\_<" (regexp-opt more-keywords 'paren) "\\_>")
+;;                                             1 font-lock-keyword-face)) 'append)
+;;     ))
 
-(add-hook 'python-mode-hook #'my/mcpyrate-syntax-highlight-setup)
+;; (add-hook 'python-mode-hook #'my/mcpyrate-syntax-highlight-setup)
 
 (defun make-ppss-easy (&optional ppss)
   "Constructor of type `ppss' from PPSS.
