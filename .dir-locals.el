983bed5ac8e493dc44b4dfce48e5da1800fb25c1
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-02-11 18:13:02 -0600

    [UPDATE] .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,36 +1,20 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((dired-mode .
-	     ((eval .
-		    (progn
-		      (defun dired-diff-with-source-file (file &optional switches)
-			(interactive
-			 (let* ((current (dired-get-filename t))
-				(target-dir "~/.emacs.d/extensions")
-				(default (s-lex-format "${target-dir}/${current}"))
-				(initial (s-lex-format "/${current}"))
-				(defaults (dired-dwim-target-defaults (list current) target-dir)))
-			   (list (minibuffer-with-setup-hook
-				     (lambda nil
-				       (setq-local minibuffer-default-add-function nil)
-				       (setq minibuffer-default defaults))
-				   (read-file-name (format "Diff %s with%s: " current
-							   (if default
-							       (s-lex-format " (default ${default})")
-							     ""))
-						   target-dir default t initial))
-				 (when current-prefix-arg
-				   (read-string "Options for diff: "
-						(if (stringp diff-switches)
-						    diff-switches
-						  (mapconcat #'identity diff-switches " ")))))))
-			(let ((current (dired-get-filename t)))
-			  (diff current file switches)))
-		      (define-key dired-mode-map (kbd "M-=") #'dired-diff-with-source-file)
-
-		      (defun mc-todo-hyphen-check (&optional arg)
-			"Keyboard macro."
-			(interactive "p")
-			(kmacro-exec-ring-item '([insert home right right right kp-subtract end insert] 0 "%d")
-					       arg)))))))
+((emacs-lisp-mode . ((eval .
+			   (progn
+			     (unless (fboundp 'mc-doc-ext)
+			       (defun mc-doc-ext (&optional arg)
+				 "Keyboard macro."
+				 (interactive "p")
+				 (kmacro-exec-ring-item
+				  '([40 100 111 99 117 109 101 110 116 45 101 120 116
+					101 110 115 105 111 110 32 34 21 24 81 69 120
+					116 101 110 115 105 111 110 58 32 return return
+					right return 34] 0 "%d")
+				  arg)))
+			     (unless (fboundp 'mc-autoload)
+			       (defun mc-autoload (&optional arg)
+				 "Keyboard macro."
+				 (interactive "p")
+				 (kmacro-exec-ring-item '(";;;###autoload" 0 "%d") arg))))))))
