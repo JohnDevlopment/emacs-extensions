f03f622e3c5e223e7a4d14321b5641147155993c
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-02-25 18:43:26 -0600

    [FIX] Extension function completion

--- extensions.el
+++ extensions.el
@@ -33,11 +33,11 @@
 
 ;; ---Loading/finding extensions
 
-(defun --extension-completion (&optional initial-input)
-  (let* (completion-ignored-extensions
-	 (path "~/.emacs.d/extensions"))
-    (list (completing-read "Load extension: "
-			   (apply-partially 'locate-file-completion-table
+(defun --extension-completion (prompt &optional initial-input)
+  (let* ((path "~/.emacs.d/extensions")
+	 completion-ignored-extensions)
+    (list (completing-read prompt
+			   (apply-partially #'locate-file-completion-table
 					    (list path)
 					    (get-load-suffixes))
 			   nil nil initial-input))))
@@ -48,23 +48,22 @@
 EXTENSION is a file '~/.emacs.d/extensions' without its file
 extension.  For example, with an extension named 'general',
 the file '~/.emacs.d/extensions/general.el' is loaded."
-  (interactive (--extension-completion))
+  (interactive (--extension-completion "Load Extenion: "))
   (load (concat "~/.emacs.d/extensions/" extension)))
 (set-advertised-calling-convention 'load-extension '(extension) "2025.02.02")
 
 (defmacro load-extension-safe (extension)
-  "Load EXTENSION.
-This calls `load-extension' but captures the error condition
-`file-missing'.  If such an error occurs, the resulting
-error is demoted to a simple message."
+  "Load EXTENSION, capturing any error and displaying it as a message."
   (cl-check-type extension string)
-  `(condition-case err (load-extension ,extension)
-     (file-missing
-      (message "%s" (error-message-string err)))))
+  ;; `(condition-case err (load-extension ,extension)
+  ;;    (file-missing
+  ;;     (message "%s" (error-message-string err))))
+  `(with-demoted-errors ,(concat "Error loading " extension ": %S")
+     (load-extension ,extension)))
 
 (defun find-extension (extension)
   "Find the Emacs Lisp source of EXTENSION."
-  (interactive (--extension-completion))
+  (interactive (--extension-completion "Find Extension: "))
   (prog1
       (switch-to-buffer (find-file-noselect
 			 (concat "~/.emacs.d/extensions/" extension ".el")))))
@@ -74,14 +73,15 @@ error is demoted to a simple message."
    (let* ((thing (thing-at-point 'sexp))
 	  (end (if (stringp thing) (length thing))))
      (set-text-properties 0 end nil thing)
-     (--extension-completion (when (stringp thing) thing))))
+     (--extension-completion "Find Extension: "
+			     (cl-ext-when (stringp thing) thing))))
   (prog1
       (switch-to-buffer (find-file-noselect
 			 (concat "~/.emacs.d/extensions/" extension ".el")))))
 
 (defun get-extension-documentation (extension)
   "Display the full documentation for EXTENION (a string)."
-  (interactive (--extension-completion))
+  (interactive (--extension-completion "Get Help For Extension: "))
   (let ((var (intern-soft (format "user-ext-%s-documentation" extension))))
     (print-expr sexp (list var extension))
     (unless var
