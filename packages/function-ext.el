866aae83eb4c6835492eeaeeecf52ddd89202766
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-03-15 06:59:03 -0500

    [UPDATE] Function: Improve fext-replace-function
    * Add advice classes
    ** before
    ** before-while
    ** before-until
    ** after-while
    ** after-until
    ** override
    ** filter-args
    ** filter-return
    * Add `:remove' keyword

--- packages/function-ext.el
+++ packages/function-ext.el
@@ -1,10 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
 (defconst user-ext-fext-valid-advice-classes
-  '(before around after activation deactivation))
+  '(before
+    before-while
+    before-until
+    around
+    after
+    after-while
+    after-until
+    activation
+    deactivation
+    override
+    filter-args
+    filter-return))
 
 (eval-when-compile
-  (require 'cl-lib))
+  (require 'cl-lib)
+  (require 'cl-ext))
 
 (defmacro fext-replace-function (symbol file arglist &optional docstring &rest body)
   "Replace the function definition of SYMBOL.
@@ -15,10 +27,13 @@ are the same as `defun', which see.
 This function has two side effects: on the first invocation
 of this macro for SYMBOL, its original definition is saved
 to SYMBOL--old.  Second, SYMBOL is declared as a function
-via `declare-function'.
+via `declare-function'.  This is just to placate the byte-
+compiler.
 
 \(fn SYMBOL FILE ARGLIST [DOCSTRING] [DECL] BODY...)"
-  (declare (indent 3) (doc-string 4))
+  (declare (indent 3) (doc-string 4)
+	   (debug (&define name stringp (&rest arg)
+			   [&optional stringp] def-body)))
   (cl-check-type file string-or-null)
   (cl-check-type symbol symbol)
   (cl-check-type arglist (or list null))
@@ -37,30 +52,52 @@ via `declare-function'.
 The syntax of `fext-defadvice' is as follows:
 
   (fext-defadvice FUNCTION (CLASS NAME [ARGLIST])
+    [KEYWORD-ARGS]
     [DOCSTRING] [INTERACTIVE-FORM]
     BODY...)
 
 FUNCTION ::= Name of the function being advised.
-CLASS ::= `before' | `around' | `after' | `activation' |
-    `deactivation'
+CLASS ::= `before' | `before-while' `before-until' |
+    `around' | `after' | `after-while' |  `after-until' |
+    `activation' | `deactivation' | `override' |
+    `filter-args' | `filter-return'
 ARGLIST ::= Argument list for the advice.  Without it,
     FUNCTION's argument list will consist of
-    \\`(&optional _args)'
+    \`(&optional _args)'
 DOCSTRING ::= Optional documentation string for defined
     function.
 INTERACTIVE-FORM ::= Optional interactive form.
 BODY ::= Any s-expression.
 
+For CLASS, see the WHERE argument of `add-function'.
+
+Keyword arguments can be provided at the beginning of BODY.
+The following keywords are supported:
+
+:remove FORM    If FORM evaluates to non-nil, the advice is
+                removed from FUNCTION.  In this case, the
+                rest of BODY is not evaluated.  CLASS and
+                NAME are used to specify what advice is
+                removed.
+
 \(fn FUNCTION (CLASS NAME [ARGLIST]) BODY...)"
   (declare (indent 2) (debug (&define name
-				      ([&or "before" "around"
-					    "after" "activation" "deactivation"]
+				      ([&or "before" "before-while"
+					    "before-until" "around" "after"
+					    "after-while" "after-until"
+					    "activation" "deactivation"
+					    "override"]
 				       name
 				       [&optional listp])
+				      [&optional ":remove" form]
 				      [&optional stringp]
 				      [&optional ("interactive" interactive)]
 				      def-body)))
   (let ((arglist '(&rest _args))
+	(remove (cl-ext-progn
+		  (cl-ext-when (eq (car-safe body) :remove)
+		    (pop body)
+		    (pop body))))
 	aname fname
 	class)
     (cl-destructuring-bind (c n &optional al) args
@@ -70,17 +107,20 @@ BODY ::= Any s-expression.
       (when al
 	(cl-check-type al list)
 	(setq arglist al)))
-    (unless (memq class user-ext-fext-valid-advice-classes)
+    (unless (memq class user-ext-fext-valid-advice-classes) ; check valid CLASS
       (signal-wrong-argument class user-ext-fext-valid-advice-classes))
-    (setq fname (intern (format "advice-ext--%S-%S" class fname))
-	  aname (intern (format "advice-%S-%S" class function))
-	  class (intern-soft (format ":%S" class)))
-    (cl-assert (not (null class)))
-    `(progn
-       (defun ,fname ,arglist ,@body)
-       (unless (advice-member-p (quote ,aname) (quote ,function))
-	 (advice-add (quote ,function) ,class (quote ,fname)
-		     (alist-ext-define 'name (quote ,aname)))))))
+    (setq fname (intern (format "advice-ext--%S-%S" class fname)) ; advice function name
+	  aname (intern (format "advice-%S-%S" class function))	  ; advice name
+	  class (intern-soft (format ":%S" class)))		  ; CLASS (e.g., ":after")
+    (if remove
+	(cl-ext-progn
+	  `(advice-remove (function ,function) (quote ,aname)))
+      (cl-assert (not (null class)))
+      `(progn
+	 (defun ,fname ,arglist ,@body)
+	 (unless (advice-member-p (quote ,aname) (quote ,function))
+	   (advice-add (quote ,function) ,class (quote ,fname)
+		       (alist-ext-define 'name (quote ,aname))))))))
 
 (provide 'function-ext)
 
