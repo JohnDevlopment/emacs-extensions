b7ab27dce5bc42351305453a36e45724148e67a9
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-03-17 15:46:22 -0500

    [UPDATE] dired-ext.el

--- dired-ext.el
+++ dired-ext.el
@@ -2,19 +2,10 @@
 
 (require 'dired)
 
-;; Enable `dired-find-alternate-file'
-(put #'dired-find-alternate-file 'disabled nil)
-
-;; Bind C-x M-d to `dired-alternate'
-(global-set-key (kbd "C-x M-d") #'dired-alternate)
-
+(eval-when-compile
+  (require 'function-ext))
 
 ;; Functions
-
-(defun dired-ext-find-alternate-updir ()
-  "In Dired, find alternate file in parent directory."
-  (interactive)
-  (find-alternate-file ".."))
 
 (defun dired-ext-quit-kill-window ()
   "Kill the current buffer and quit the window."
@@ -36,6 +27,38 @@ individually, so list-sorting options are pointless."
 	(newbuf (dired-noselect dirname switches)))
     (pop-to-buffer-same-window newbuf)
     (kill-buffer curbuf)))
+
+(defun dired-ext-find-alternate-updir ()
+  "In Dired, find alternate file in parent directory."
+  (interactive)
+  (when (derived-mode-p 'dired-mode)
+    ;; (dired-listing-switches dired-actual-switches)
+    ;; (find-alternate-file "..")
+    ;; dired-actual-switches
+    (dired-alternate "..")))
+
+(defun dired-dirs (dirname &optional _switches)
+  "\"Edit\" directory DIRNAME like `dired', but list only directories.
+
+DIRNAME is either a string or a cons: as a string, the name
+of the directory to edit; as a cons, the first element is
+the directory, and the tail is a list of files to make
+directory entries for."
+  (interactive (dired-read-dir-and-switches ""))
+  (let ((newbuf (dired-noselect
+		 (mapcar (lambda (str) (f-join dirname str)) '("*/" ".*/")) "-adl")))
+    (pop-to-buffer-same-window newbuf)))
+
+;; Keybinds
+
+;; Enable `dired-find-alternate-file'
+(put #'dired-find-alternate-file 'disabled nil)
+
+;; Bind C-x M-d to `dired-alternate'
+(global-set-key (kbd "C-x M-d") #'dired-alternate)
+
+;; Bind C-x C-M-d to `dired-dirs'
+(global-set-key (kbd "C-x C-M-d") #'dired-dirs)
 
 ;; Hook
 
