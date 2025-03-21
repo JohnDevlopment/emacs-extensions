7af33731c7abb867ab36bc27f7f5ec7b43f2ccbf
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-03-21 12:31:33 -0500

    [REFACTOR] Jdesktop-bootstrap

--- jdesktop-bootstrap.el
+++ jdesktop-bootstrap.el
@@ -1,34 +1,14 @@
 ;; *- lexical-binding: t; -*-
 
 (eval-when-compile
-  (require 'use-package)
-  (declare-function jdesktop-default-bindings "jdesktop"))
+  (require 'use-package))
 
-(eval-and-compile
-  (defun user-ext-desktop-load-path--setter (symbol value)
-    (set-default symbol value)
-    (load-jdesktop-package))
-
-  (defcustom user-ext-desktop-load-path ""
-    "Load path for `use-package' of package `jdesktop'."
-    :type 'file
-    :set #'user-ext-desktop-load-path--setter
-    :group 'desktop-ext))
-
-(defun load-jdesktop-package ()
-  "Load `jdesktop' package."
-  (let ((path user-ext-desktop-load-path))
-    (cl-ext-when (file-exists-p path)
-      (add-to-list 'load-path path))
-    (use-package jdesktop
-      :if (locate-library "jdesktop")
-      :defer t
-      :functions
-      jdesktop-default-bindings
-      :config
-      (jdesktop-default-bindings))))
-
-;; (cl-prettyprint (symbol-function 'load-jdesktop-package))
+(use-package jdesktop
+  :load-path "~/github/desktop-plus/"
+  :functions
+  jdesktop-default-bindings
+  :config
+  (jdesktop-default-bindings))
 
 (provide 'jdesktop-bootstrap)
 ;;; jdesktop-bootstrap.el ends here
