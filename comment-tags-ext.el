4b49894e91cfaf1414dddcafbe3dae615bec7cc2
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-04-18 21:36:04 -0500

    [UPDATE] Comment-tags: Rework global mode
    Instead of timers, we use the same method as
    company mode; that is, a list to (ex/in)clude
    modes in which the minor mode is enabled.

--- comment-tags-ext.el
+++ comment-tags-ext.el
@@ -2,23 +2,43 @@
 
 (require 'comment-tags)
 
+(eval-when-compile
+  (require 'cl-ext))
+
 ;; Advice
 
 (fext-defadvice comment-tags-list-tags-buffer (after list-comment-tags-buffer)
+  "Enable View mode and bind `kill-and-quit' to k."
   (with-current-buffer comment-tags-temp-buffer-name
-    ;; (local-set-key (kbd "C-c c q") #'quit-window)
-    (view-mode 1)))
+    (activate-view-mode)
+    (local-set-key (kbd "k") #'kill-and-quit)))
 
 ;; Variables
 
-(defcustom user-ext-global-comment-tags-exclude
-  nil
-  "List of buffers to exclude from `comment-tags-mode'."
-  :group 'global-comment-tags
-  :type '(repeat string))
+(defgroup comment-tags-ext nil
+  "Comment tags extension."
+  :group 'user-extensions)
 
-(defvar global-comment-tags-regexp (comment-tags--make-regexp)
-  "Regular expression for matching TODO-ish comments.")
+(defcustom user-ext-global-comment-tags-exclude nil
+  "Modes for which `comment-tags-mode' is turned on by `global-comment-tags-mode'.
+If nil, `comment-tags-mode' is not enabled for any mode.  If
+t, it is enabled for every mode.  Otherwise, this is a list
+of `major-mode' symbols for which `comment-tags-mode' should
+be automatically turned on.  The sense of the list is
+negated if it starts with `not'."
+  :type '(choice (const :tag "none" nil)
+		 (const :tag "all" t)
+		 (set :tag "modes"
+		      :value (not)
+		      (const :tag "Except" not)
+		      (repeat :inline t (symbol :tag "mode"))))
+  :group 'comment-tags-ext)
+
+(defvar user-ext-global-comment-tags-regexp
+  (rx (+ (syntax comment-start)) (* (syntax whitespace))
+      (regexp (comment-tags--make-regexp)))
+  "Regular expression for matching TODO-ish comments.
+This is set once the global minor mode is enabled.")
 
 ;; Functions
 
@@ -26,20 +46,24 @@
 (defun comment-tags-mode-turn-on ()
   "Turn on `comment-tags-mode' if the buffer has TODO-ish comments."
   (interactive)
-  (let ((buf (current-buffer))
-	flag)
-    (unless (memq buf user-ext-global-comment-tags-exclude)
-      (save-excursion
-	(goto-char (point-min))
-	(setq flag (re-search-forward global-comment-tags-regexp nil t)))
-      (when flag
-	;; (message "`comment-tags-mode' enabled in %s" buf)
-	(comment-tags-mode 1)))))
+  (let ((modes user-ext-global-comment-tags-exclude))
+    (if (eq (car modes) 'not)
+	(cl-ext-progn
+	  (cl-ext-unless (cl-member-if #'derived-mode-p (cdr modes))
+	      (comment-tags-mode)))
+      (cl-ext-when (cl-member-if #'derived-mode-p modes)
+	  (comment-tags-mode)))))
 
 ;;;###autoload
 (define-globalized-minor-mode global-comment-tags-mode
-  comment-tags-mode comment-tags-mode-turn-on)
+  comment-tags-mode comment-tags-mode-turn-on
+  :group 'comment-tags-ext)
 
 (provide 'comment-tags-ext)
 
 ;;; comment-tags-ext ends here
+
+;; Local Variables:
+;; eval: (local-lambda-define-self-insert-command fgprefix "global-comment-tags-")
+;; eval: (local-lambda-define-self-insert-command vgprefix "user-ext-global-comment-tags-")
+;; End:
