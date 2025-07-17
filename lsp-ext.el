d1c1f39f58bdc871b803e0b495e19e3302b9d5e8
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-16 20:12:08 -0500

    [REFACTOR] Lsp
    * Remove commented code
    * Add autoload cookies
    * Obsolete aliases for functions
    * (Sub)sections

--- lsp-ext.el
+++ lsp-ext.el
@@ -1,17 +1,27 @@
 ;;-*- lexical-binding: t; -*-
 
-(require 'function-ext)
-(require 'lsp-mode)
-(require 'gv)
 (require 'cl-lib)
+(require 'debug-ext)
+(require 'faces)
+(require 'function-ext)
+(require 'gv)
+(require 'lsp-mode)
+
+(eval-when-compile
+  (require 'cl-ext)
+  (require 'dash)
+  (declare-function lsp-headerline--workspace-root "lsp-headerline")
+  (declare-function lsp-headerline--path-up-to-project-root "lsp-headerline")
+  (declare-function lsp-headerline--build-file-string "lsp-headerline"))
 
 (eval-and-compile
-  (setq lsp-keymap-prefix "C-c c l")
-  (define-key lsp-mode-map (kbd "C-c l T i") #'lsp-inlay-hints-mode)
-  (define-key lsp-mode-map (kbd "<double-mouse-1>") #'imenu)
-  (define-key lsp-mode-map (kbd "M-R") #'revert-buffer))
+  (eval-after-require lsp-mode
+    (setq lsp-keymap-prefix "C-c c l")
+    (define-key lsp-mode-map (kbd "C-c l T i") #'lsp-inlay-hints-mode)
+    (define-key lsp-mode-map (kbd "<double-mouse-1>") #'imenu)
+    (define-key lsp-mode-map (kbd "M-R") #'revert-buffer)))
 
-;; Customizations
+;; ### Customizations
 
 (defgroup lsp-ext nil
   "LSP extension."
@@ -30,12 +40,12 @@
   :type '(repeat function)
   :group 'lsp-ext)
 
-;; Variables
+;; ### Variables
 
 (defvar user-ext-lsp-temporary-workspace-folders nil
   "Folders added with `lsp-workspace-folders-add-temp'.")
 
-;; Advice
+;; ### Advice
 
 (fext-defadvice lsp-execute-code-action (after lsp-code-action)
   "Called after `lsp-execute-code-action'."
@@ -47,45 +57,18 @@ This is supposed to be called after `lsp--before-save'."
   (unless (memq major-mode user-ext-lsp-safe-inlay-modes)
     (lsp-inlay-hints-mode -1)))
 
-;; Functions
 
-;; (when nil
-;;   (defun lsp-fix-lsp-flycheck-face-errors ()
-;;     (interactive)
-;;     (defface lsp-flycheck-info-unnecessary
-;;       '((t (:inherit 'lsp-flycheck-info-unnecessary-face)))
-;;       "Face for the sideline for symbols not used.
-;; Possibly redundant of face `lsp-flycheck-info-unnecessary-face'."
-;;       :group 'lsp-ui-sideline)
-;;     (defface lsp-flycheck-warning-unnecessary
-;;       '((t (:inherit 'lsp-flycheck-warning-unnecessary-face)))
-;;       "Face for the sideline for symbols not used.
-;; Possibly redundant of face `lsp-flycheck-warning-unnecessary-face'."
-;;       :group 'lsp-ui-sideline))
+;; ### Functions
 
-;;   (defun lsp-flycheck-define-error-level (name face)
-;;     "Define a Flycheck error level with NAME and FACE."
-;;     (let ((category (intern (format "%s-category" name))))
-;;       (setf (get category 'face) face
-;; 	    (get category 'priority) 100)
-;;       (flycheck-define-error-level name
-;; 	:overlay-face face
-;; 	:evaporate t
-;; 	:color 'blue)))
-
-;;   (lsp-flycheck-define-error-level )
-
-;;   (symbol-plist 'lsp-flycheck-info-unnecessary))
-
-;; (my-lsp-flycheck-define-error-level "lsp-flycheck-info-unnecessary" 'lsp-flycheck-info-unnecessary)
-
+;;;###autoload
 (defun kill-lsp-buffers ()
   "Kill all buffers that have to do with function `lsp-mode'."
   (interactive)
   (dolist (brx user-ext-lsp-buffers-to-kill)
     (kill-buffers brx)))
 
-(defun lsp--delete-temp-workspace-folders ()
+;;;###autoload
+(defun lsp-ext--delete-temp-workspace-folders ()
   "Remove temporary folders from the LSP workspace.
 
 Remove the contents of `user-ext-lsp-temporary-workspace-folders'
@@ -93,8 +76,11 @@ from the workspace list.  Effectively, this removes
 temporary folders from the workspace."
   (dolist (dir user-ext-lsp-temporary-workspace-folders)
     (lsp-workspace-folders-remove dir)))
+(define-obsolete-function-alias 'lsp--delete-temp-workspace-folders
+  'lsp-ext--delete-temp-workspace-folders "2025-06-03")
 
-(defun lsp-workspace-folders-remove-list ()
+;;;###autoload
+(defun lsp-ext-workspace-folders-remove-list ()
   "Call `lsp-workspace-folders-remove' one or more times in a loop."
   (interactive)
   (let (answer (flag t))
@@ -104,8 +90,11 @@ temporary folders from the workspace."
       (if (= answer ?y)
 	  (call-interactively 'lsp-workspace-folders-remove)
 	(setq flag nil)))))
+(define-obsolete-function-alias 'lsp-workspace-folders-remove-list
+  'lsp-ext-workspace-folders-remove-list "2025-06-03")
 
-(defun lsp-workspace-blocklist-add (project-root)
+;;;###autoload
+(defun lsp-ext-workspace-blocklist-add (project-root)
   "Add PROJECT-ROOT to the workspace blocklist."
   (interactive
    (list (read-directory-name "Select folder to add: "
@@ -113,29 +102,40 @@ temporary folders from the workspace."
   (setf (lsp-session-folders-blocklist (lsp-session))
 	(cons project-root (lsp-session-folders-blocklist (lsp-session))))
   (lsp--persist-session (lsp-session)))
+(define-obsolete-function-alias 'lsp-workspace-blocklist-add
+  'lsp-ext-workspace-blocklist-add "2025-06-03")
 
-(defun lsp-workspace-blocklist-remove-all ()
+;;;###autoload
+(defun lsp-ext-workspace-blocklist-remove-all ()
   "Remove all folders in the workspace blocklist."
   (interactive)
   (setf (lsp-session-folders-blocklist (lsp-session)) nil)
   (lsp--persist-session (lsp-session)))
+(define-obsolete-function-alias 'lsp-workspace-blocklist-remove-all
+  'lsp-ext-workspace-blocklist-remove-all "2025-06-03")
 
-(defun lsp-workspace-folders-add-temp (project-root)
+;;;###autoload
+(defun lsp-ext-workspace-folders-add-temp (project-root)
   "Temporarily add PROJECT-ROOT to the list of workspace folders."
   (interactive
    (list (read-directory-name "Select folder to add: "
 			      (or (lsp--suggest-project-root) default-directory) nil t)))
   (lsp-workspace-folders-add project-root)
   (cl-pushnew project-root user-ext-lsp-temporary-workspace-folders :test #'string=))
+(define-obsolete-function-alias 'lsp-workspace-folders-add-temp
+  'lsp-ext-workspace-folders-add-temp "2025-06-03")
 
-;; Keymaps
+;; ### Keymaps
 
 (define-key lsp-mode-map (kbd "<C-return>") #'lsp-find-definition)
 
-;; Exit hook
-(add-hook 'kill-emacs-hook #'lsp--delete-temp-workspace-folders)
+;; ### Hooks
 
-;; (add-hook 'lsp-after-open-hook #'lsp-origami-try-enable)
+;;;###autoload
+(add-hook 'kill-emacs-hook #'lsp-ext--delete-temp-workspace-folders)
+
+;;;###autoload
+(add-hook 'lsp-configure-hook #'lsp-ext--start-hook)
 
 (provide 'lsp-ext)
 
