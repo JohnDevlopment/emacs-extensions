491cc8d4d2e7cd629e8c567241ae3bd1f87dcd20
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-16 19:47:57 -0500

    [UPDATE] Ibuffer: Override ibuffer-visit-buffer

--- ibuffer-ext.el
+++ ibuffer-ext.el
@@ -13,9 +13,22 @@
 (fext-defadvice ibuffer-do-view (after ibuffer-do-view)
   (view-mode 1))
 
-(fext-defadvice ibuffer-visit-buffer (after ibuffer-visit-buffer)
-  "Bury the IBuffer buffer after visiting the selected buffer."
-  (bury-buffer (get-buffer "*Ibuffer*")))
+;; (fext-defadvice ibuffer-visit-buffer (after ibuffer-visit-buffer)
+;;   "Bury the IBuffer buffer after visiting the selected buffer."
+;;   )
+
+(fext-replace-function ibuffer-visit-buffer "ibuffer-ext" (&optional single)
+  "Visit the buffer on this line.
+If optional argument SINGLE is non-nil, then also ensure
+there is only one window.
+
+When called interactively, SINGLE is the prefix argument."
+  (interactive "P")
+  (let ((buf (ibuffer-current-buffer t)))
+    (bury-buffer)
+    (switch-to-buffer buf)
+    (when single
+      (delete-other-windows))))
 
 (define-key ibuffer-mode-map (kbd "/ T") #'ibuffer-ext-toggle-current-filter-group)
 
