43419b51dc889654a57666f3e5a18de43fcbd5e2
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-17 21:40:12 -0500

    [REFACTOR] Org: Organize into (sub)sections

--- polymode-ext.el
+++ polymode-ext.el
@@ -2,23 +2,15 @@
 
 (require 'polymode)
 
-;; ### Modes
-
-;; --- Host modes
+;; ### Host modes
 
 (define-hostmode poly-org-hostmode
   :mode #'org-mode
   :protect-font-lock t)
 
-;; --- Inner modes
 
-(define-innermode poly-org-eval-innermode
-  nil
-  "Innermode for file variables."
-  :head-matcher "^# eval: "
-  :tail-matcher "$"
-  :mode #'emacs-lisp-mode
-  :allow-nested nil)
+
+;; ### Inner modes
 
 (define-innermode poly-generic-eval-innermode
   nil
@@ -28,19 +20,41 @@
   :mode #'emacs-lisp-mode
   :allow-nested nil)
 
-;; --- Poly modes
+;; --- Org
 
-(define-polymode poly-org-file-variables-mode
+(define-innermode poly-org-eval-innermode
   nil
-  "A variation of `org-mode' for Poly mode."
-  :hostmode 'poly-org-hostmode
-  :innermodes '(poly-org-eval-innermode))
+  "Innermode for file variables."
+  :head-matcher "^# eval: "
+  :tail-matcher "$"
+  :mode #'emacs-lisp-mode
+  :allow-nested nil)
+
+(define-innermode poly-org-macro-eval-innermode
+  nil
+  "Innermode for Emacs Lisp code in macro replacements."
+  :head-matcher "^#\\+macro: .*?(eval"
+  :head-mode 'host
+  :tail-matcher ")\n"
+  :tail-mode 'host
+  :mode #'emacs-lisp-mode
+  :allow-nested nil)
+
+
+  nil
+;; ### Poly modes
 
 (define-polymode poly-emacs-lisp-file-variables-mode
   nil
   "A variation of `emacs-lisp-mode' for Polymode."
   :hostmode 'poly-emacs-lisp-hostmode
   :innermodes '(poly-generic-eval-innermode))
+
+(define-polymode poly-org-mode
+  nil
+  "A variation of `org-mode' for Poly mode."
+  :hostmode 'poly-org-hostmode
+  :innermodes '(poly-org-eval-innermode))
 
 ;; ### Advice
 
