af15fd32568e1281ce1ad51bccc81e0907776ebb
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-12-11 14:06:09 -0600

    [UPDATE] bookmark-menu-ext.el

--- bookmark-menu-ext.el
+++ bookmark-menu-ext.el
@@ -20,30 +20,33 @@ This kills the current buffer unless it's modified (see
     (run-with-idle-timer 0.5 nil #'kill-buffer-if-not-modified curbuf)
     (bookmark-bmenu-this-window)))
 
-(fext-replace-function bookmark-completing-read "bookmark-menu-ext"
-		       (prompt &optional default)
-  "Prompting with PROMPT, read a bookmark name in completion.
+(--ignore
+ (fext-replace-function bookmark-completing-read "bookmark-menu-ext"
+			(prompt &optional default)
+   :remove
+   "Prompting with PROMPT, read a bookmark name in completion.
 PROMPT will get a \": \" stuck on the end no matter what, so you
 probably don't want to include one yourself.
 Optional arg DEFAULT is a string to return if the user input is empty.
 If DEFAULT is nil then return empty string for empty input."
-  (bookmark-maybe-load-default-file) ; paranoia
-  (if (listp last-nonmenu-event)
-      (bookmark-menu-popup-paned-menu t prompt
-				      (if bookmark-sort-flag
-					  (sort (bookmark-all-names)
-						'string-lessp)
-					(bookmark-all-names)))
-    (let* ((completion-ignore-case bookmark-completion-ignore-case)
-           (default (unless (equal "" default) default))
-	   (prompt (concat prompt (if default
-                                      (format " (%s): " default)
-                                    ": "))))
-      (ido-completing-read prompt
-			   (-sort #'string<
-				  (cl-loop for (bookmark . _spec) in bookmark-alist
-					   collect (cl-ext-progn
-						     bookmark)))))))
+   (bookmark-maybe-load-default-file) ; paranoia
+   (if (listp last-nonmenu-event)
+       (bookmark-menu-popup-paned-menu t prompt
+				       (if bookmark-sort-flag
+					   (sort (bookmark-all-names)
+						 'string-lessp)
+					 (bookmark-all-names)))
+     (let* ((completion-ignore-case bookmark-completion-ignore-case)
+            (default (unless (equal "" default) default))
+	    (prompt (concat prompt (if default
+                                       (format " (%s): " default)
+                                     ": "))))
+       (ido-completing-read prompt
+			    (-sort #'string<
+				   (cl-loop for (bookmark . _spec) in bookmark-alist
+					    collect (cl-ext-progn
+						      bookmark)))))))
+ t)
 
 ;;;###autoload
 (defun bookmark-bmenu--extra-hook () t)
