375167929703e2b3194227a0072334e6b994b630
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-01-19 15:29:55 -0600

    [ADD] Main: Re-add eglot-ext

---
+++ eglot-ext.el
@@ -0,0 +1,198 @@
+;; -*- lexical-binding: t; -*-
+
+(check-emacs-minimum-version "27.4")
+
+(require 'eglot nil t)
+(require 'transient nil t)
+
+(extension-check-requires eglot transient)
+
+
+;; ### Customization
+
+(defgroup eglot-ext nil
+  "Eglot extension."
+  :group 'user-extensions)
+
+(defcustom user-ext-eglot-manage-hook nil
+  "A hook run during the extension hook."
+  :group 'eglot-ext
+  :type 'hook)
+
+
+;; ### Functions
+
+(defun eglot-ext-kill-buffers (arg)
+  "Shutdown Eglot and maybe kill all of its buffers.
+If ARG is non-nil, shutdown all Eglot servers and kill all
+of its buffers.  Otherwise, shutdown one of its servers."
+  (interactive "P")
+  (let ((rx (rx (or (seq "*Flymake" (* nonl))))))
+    (if arg
+	(cl-ext-progn
+	  (eglot-shutdown-all)
+	  (kill-buffers rx))
+      (call-interactively #'eglot-shutdown))))
+
+(fext-defadvice eglot-show-workspace-configuration
+    (after eglot-show-workspace-configuration)
+  (let ((buffer (get-buffer "*EGLOT workspace configuration*")))
+    (cl-assert buffer)
+    (with-current-buffer buffer
+      (activate-view-mode 1))))
+
+
+;; ### Keymaps
+
+(--ignore
+  (cl-ext-progn
+    (define-key eglot-mode-map (kbd "C-c l") nil)
+    (define-key eglot-mode-map (kbd "<mouse-3>") nil)
+    ;; Workspace map
+    (define-key user-ext-eglot-workspace-map (kbd "d") nil)
+    (define-key user-ext-eglot-workspace-map (kbd "q") nil)
+    (define-key user-ext-eglot-workspace-map (kbd "M-q") nil)
+    (define-key user-ext-eglot-workspace-map (kbd "k") nil)
+
+    ;; Code actions map
+    (define-key user-ext-eglot-code-actions-map (kbd "a") nil)
+    (define-key user-ext-eglot-code-actions-map (kbd "i") nil)
+    (define-key user-ext-eglot-code-actions-map (kbd "q") nil)
+    (define-key user-ext-eglot-code-actions-map (kbd "n") nil)
+    (define-key user-ext-eglot-code-actions-map (kbd "x") nil)
+    (define-key user-ext-eglot-code-actions-map (kbd "w") nil)
+
+    ;; Rename map
+    (define-key user-ext-eglot-rename-map (kbd "r") nil)
+
+    ;; Format map
+    (define-key user-ext-eglot-format-map (kbd "r") nil)
+    (define-key user-ext-eglot-format-map (kbd "=") nil)
+
+    (define-key user-ext-eglot-command-map (kbd "w") nil)
+    (define-key user-ext-eglot-command-map (kbd "a") nil)
+    (define-key user-ext-eglot-command-map (kbd "r") nil)
+    (define-key user-ext-eglot-command-map (kbd "=") nil)
+
+    (setq user-ext-eglot-command-map nil)
+    (setq user-ext-eglot-workspace-map nil)
+    (setq user-ext-eglot-code-actions-map nil)
+    (setq user-ext-eglot-rename-map nil)
+    (setq user-ext-eglot-format-map nil))
+
+  (dolist (symbol '(user-ext-eglot-code-actions-map
+		    user-ext-eglot-command-map
+		    user-ext-eglot-format-map
+		    user-ext-eglot-rename-map
+		    user-ext-eglot-workspace-map))
+    (--destroy-function symbol)
+    (--destroy-variable symbol))
+
+  t)
+
+(--ignore
+  (eval-and-compile
+    ;; Command map
+    (define-prefix-command 'user-ext-eglot-command-map)
+    (define-key eglot-mode-map (kbd "C-c l") #'user-ext-eglot-command-map)
+
+    ;; Workspace
+    (define-prefix-command 'user-ext-eglot-workspace-map)
+    (define-key user-ext-eglot-command-map (kbd "w") #'user-ext-eglot-workspace-map)
+    ;; (define-key user-ext-eglot-workspace-map (kbd "d") #'eglot-show-workspace-configuration)
+    ;; (define-key user-ext-eglot-workspace-map (kbd "q") #'eglot-shutdown)
+    ;; (define-key user-ext-eglot-workspace-map (kbd "M-q") #'eglot-shutdown-all)
+    ;; (define-key user-ext-eglot-workspace-map (kbd "k") #'eglot-ext-kill-buffers)
+
+    ;; Code actions
+    ;; (define-prefix-command 'user-ext-eglot-code-actions-map)
+    ;; (define-key user-ext-eglot-command-map (kbd "a") #'user-ext-eglot-code-actions-map)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "a") #'eglot-code-actions)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "i") #'eglot-code-action-organize-imports)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "q") #'eglot-code-action-quickfix)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "n") #'eglot-code-action-inline)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "x") #'eglot-code-action-extract)
+    ;; (define-key user-ext-eglot-code-actions-map (kbd "w") #'eglot-code-action-rewrite)
+
+    ;; Rename
+    ;; (define-prefix-command 'user-ext-eglot-rename-map)
+    ;; (define-key user-ext-eglot-command-map (kbd "r") #'user-ext-eglot-rename-map)
+    ;; (define-key user-ext-eglot-rename-map (kbd "r") #'eglot-rename)
+
+    ;; Format
+    (define-prefix-command 'user-ext-eglot-format-map)
+    (define-key user-ext-eglot-command-map (kbd "=") #'user-ext-eglot-format-map)
+    ;; (define-key user-ext-eglot-format-map (kbd "r") #'eglot-format)
+    ;; (define-key user-ext-eglot-format-map (kbd "=") #'eglot-format-buffer)
+
+    (define-key eglot-mode-map (kbd "<mouse-3>") #'imenu))
+  t)
+
+(transient-define-prefix eglot-ext-main ()
+  "Run an Eglot command."
+  ["Workspace"
+   ("w d" "Show workspace configuration" eglot-show-workspace-configuration)
+   ("w q" "Shutdown a workspace" eglot-shutdown)
+   ("w M-q" "Shutdown all workspaces" eglot-shutdown-all)
+   ("w k" "Kill all Eglot-related buffers" eglot-ext-kill-buffers)
+   " "
+   "Code Actions"
+   ("a a" "Do a code action" eglot-code-actions)
+   ("a i" "Organize imports" eglot-code-action-organize-imports)
+   ("a q" "Do a quickfix action" eglot-code-action-quickfix)
+   ("a n" "Do an inline action" eglot-code-action-inline)
+   ("a x" "Do an extract action" eglot-code-action-extract)
+   ("a w" "Do a rewrite action" eglot-code-action-rewrite)
+   " "
+   "Rename"
+   ("r r" "Rename symbol at point" eglot-rename)
+   " "
+   "Feature"
+   ("T i" "Toggle inlay hints" eglot-inlay-hints-mode)
+   " "
+   "Formatting"
+   ("= r" "Format the region" eglot-format)
+   ("= =" "Format the buffer" eglot-format-buffer)
+   " "
+   "Help"
+   ("h h" "Show help for symbol at point" eldoc)
+   " "
+   "Misc."
+   (". c" "Customize group" (lambda ()
+			      (interactive)
+			      (customize-group 'eglot-ext)))])
+
+(define-key eglot-mode-map (kbd "<mouse-3>") #'imenu)
+
+(define-key eglot-mode-map (kbd "C-c l") #'eglot-ext-main)
+
+
+;; ### Hooks
+
+;;;###autoload
+(defun eglot-managed--extra-hook ()
+  (let ((wf (lambda (f)
+	      (prog1 nil
+		(with-demoted-errors "`user-ext-eglot-manage-hook' error: %S"
+		  (funcall f))))))
+    (run-hook-wrapped 'user-ext-eglot-manage-hook wf))
+  ;; (add-hook 'eglot-managed-mode-hook function nil t)
+  )
+
+(--ignore
+  (defun temp ()
+    (eglot--update-hints (point-min) (point-max)))
+  t)
+
+;;;###autoload
+(add-hook 'eglot-managed-mode-hook #'eglot-managed--extra-hook)
+
+
+(extension-provide 'eglot-ext)
+;;; eglot-ext.el ends here
+
+;; Local Variables:
+;; eval: (abbrev-ext-install-local-abbrev-functions)
+;; eval: (abbrev-ext-define-local-abbrev "ex" "eglot-ext")
+;; eval: (abbrev-ext-define-local-abbrev "ux" "user-ext-eglot")
+;; End:
