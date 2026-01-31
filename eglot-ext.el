78fa6221a89b50b7364f9604343c4352f4ff3986
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-01-31 07:13:38 -0600

    [FEATURE] Eglot: Add custom after-save hook

--- eglot-ext.el
+++ eglot-ext.el
@@ -19,6 +19,11 @@
   :group 'eglot-ext
   :type 'hook)
 
+(defcustom user-ext-eglot-after-save-hook nil
+  "A hook run after an Eglot-managed buffer is saved."
+  :group 'eglot-ext
+  :type 'hook)
+
 (defcustom user-ext-eglot-semantic-tokens-mode t
   "Whether to enable `eglot-semantic-tokens-mode'."
   :group 'eglot-ext
@@ -195,8 +200,12 @@ of its buffers.  Otherwise, shutdown one of its servers."
 
 ;; ### Hooks
 
+(defun eglot-ext--run-after-save-hook ()
+  (run-hooks 'user-ext-eglot-after-save-hook))
+
 ;;;###autoload
 (defun eglot-managed--extra-hook ()
+  (add-hook 'after-save-hook #'eglot-ext--run-after-save-hook nil t)
   (eglot-semantic-tokens-mode
    (if user-ext-eglot-semantic-tokens-mode 1 0))
   (eval-after-require tree-sitter-hl
