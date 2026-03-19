032948b21b148db0b3a706e23b7f12224adcfe1c
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-03-19 17:49:52 -0500

    [ADD] Compat-28: replace-regexp-in-region function

--- compat-28-ext.el
+++ compat-28-ext.el
@@ -17,5 +17,41 @@ Duplicates are removed."
           (push item result))))
     (nreverse result)))
 
+(compat-defun replace-regexp-in-region (regexp replacement &optional start end)
+  "Replace REGEXP with REPLACEMENT in the region from START to END.
+The number of replaced occurrences are returned, or nil if REGEXP
+doesn't exist in the region.
+
+If START is nil, use the current point.  If END is nil, use `point-max'.
+
+Comparisons and replacements are done with fixed case.
+
+REPLACEMENT can use the following special elements:
+
+  `\\&' in NEWTEXT means substitute original matched text.
+  `\\N' means substitute what matched the Nth `\\(...\\)'.
+       If Nth parens didn't match, substitute nothing.
+  `\\\\' means insert one `\\'.
+  `\\?' is treated literally."
+  (if start
+      (when (< start (point-min))
+        (error "Start before start of buffer"))
+    (setq start (point)))
+  (if end
+      (when (> end (point-max))
+        (error "End after end of buffer"))
+    (setq end (point-max)))
+  (save-excursion
+    (goto-char start)
+    (save-restriction
+      (narrow-to-region start end)
+      (let ((matches 0)
+            (case-fold-search nil))
+        (while (re-search-forward regexp nil t)
+          (replace-match replacement t)
+          (setq matches (1+ matches)))
+        (and (not (zerop matches))
+             matches)))))
+
 (extension-provide 'compat-28-ext)
 ;;; compat-28-ext.el ends here
