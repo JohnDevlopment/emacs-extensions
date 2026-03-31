653572c37bdbebc8082ae083f062b40a5750f78a
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-03-31 14:09:25 -0500

    [UPDATE] General: Move tree-sitter-ext-no-eln into hack

--- general.el
+++ general.el
@@ -124,33 +124,6 @@ this returns nil."
 	 (when (memq function hook-value)
 	   (cons function local)))))
 
-;; HACK: Delete tree-sitter-ext eln cache
-(defun tree-sitter-ext-no-eln ()
-  "HACK: Delete tree-sitter-ext eln cache."
-  (interactive)
-  (let ((sfile "~/.emacs.d/extensions/tree-sitter-ext.el")
-	(elc-file "~/.emacs.d/extensions/tree-sitter-ext.elc")
-	(eln-file (car-safe (f-glob "tree-sitter-ext-*.eln"
-				    "~/.emacs.d/eln-cache/29.4-13c1224a/"))))
-    (when (and eln-file (f-exists-p eln-file))
-      (delete-file eln-file)
-      (message "Deleted %s" eln-file))
-    (when (f-exists-p elc-file)
-      (delete-file elc-file)
-      (message "Deleted %s" elc-file))
-    (load-extension-safe "tree-sitter-ext")
-    (unless (get-buffer "tree-sitter-ext.el")
-      (find-extension "tree-sitter-ext"))
-    (message "Compiling after 2 seconds")
-    (run-with-idle-timer 2 nil
-			 (lambda ()
-			   (emacs-lisp-byte-compile-and-load)
-			   (sleep-for 2)
-			   (if (fboundp 'jdesktop-clear)
-			       (with-suppressed-warnings ((unresolved jdesktop-clear))
-				 (jdesktop-clear))
-			     (desktop-clear))))))
-
 
 ;; --- General commands
 
